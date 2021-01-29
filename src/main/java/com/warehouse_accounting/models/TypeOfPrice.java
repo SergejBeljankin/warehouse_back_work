5109a8bdf80cdac42f4975304878a1cce4538d7f
Dmitry <dtraider@mail.ru>
2021-01-29 21:22:21 +0300

    add TypeOfPrice

---
+++ src/main/java/com/warehouse_accounting/models/TypeOfPrice.java
@@ -0,0 +1,12 @@
+package com.warehouse_accounting.models;
+
+import lombok.Data;
+
+
+
+@Data
+public class TypeOfPrice {
+    private Long id;
+    private String name;
+    private String sortNumber;
+}
