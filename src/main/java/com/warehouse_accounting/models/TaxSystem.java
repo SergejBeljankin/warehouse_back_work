fbe25e477f316a40924ae4608645cd00e8755eef
Akromo <zloekrevedko@yandex.ru>
2021-01-30 23:07:42 +0300

    Вернул NoArgsConstructor по просьбе Uriy Anatienko

--- src/main/java/com/warehouse_accounting/models/TaxSystem.java
+++ src/main/java/com/warehouse_accounting/models/TaxSystem.java
@@ -1,7 +1,9 @@
 package com.warehouse_accounting.models;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class TaxSystem {
     private Long id;
     private String name;
