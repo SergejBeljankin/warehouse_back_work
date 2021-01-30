33725fef2d644ba789c836623a38323456271340
User <anatienko.yv@gmail.com>
2021-01-30 22:47:45 +0300

    change lombok annotation

--- src/main/java/com/warehouse_accounting/models/dto/Position.java
+++ src/main/java/com/warehouse_accounting/models/dto/Position.java
@@ -1,8 +1,10 @@
 package com.warehouse_accounting.models.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Position {
     private Long id;
     private String name;
