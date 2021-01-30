67f902173de5166e16ac76b197d4f77722d72bee
Dmitry <dtraider@mail.ru>
2021-01-30 16:12:43 +0300

    add NoArgsConstructor

--- src/main/java/com/warehouse_accounting/models/TypeOfPrice.java
+++ src/main/java/com/warehouse_accounting/models/TypeOfPrice.java
@@ -1,10 +1,11 @@
 package com.warehouse_accounting.models;
 
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 
 
 @Data
+@NoArgsConstructor
 public class TypeOfPrice {
     private Long id;
     private String name;
