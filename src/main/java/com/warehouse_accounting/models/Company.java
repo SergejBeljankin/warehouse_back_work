4ed43c80bbfd53ea0684d15ad5714442c67a4a52
User <anatienko.yv@gmail.com>
2021-02-01 18:34:37 +0300

    add model Company

---
+++ src/main/java/com/warehouse_accounting/models/Company.java
@@ -0,0 +1,75 @@
+package com.warehouse_accounting.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "company")
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(unique = true)
+    private String inn;
+
+    @Column
+    private String sortNumber;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String fax;
+
+    @Column
+    private String email;
+
+    @Column
+    private Boolean payerVat;
+
+    @Column
+    private String address;
+
+    @Column
+    private String commentToAddress;
+
+    @Column
+    private String leader;
+
+    @Column
+    private String leaderManagerPosition;
+
+    @Column
+    private String leaderSignature;
+
+    @Column
+    private String chiefAccountant;
+
+    @Column
+    private String chiefAccountantSignature;
+
+    @Column
+    private String stamp;
+
+//    @OneToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "company_id")
+//    private LegalDetail legalDetail;
+//    (OneToOne, LAZY) дождаться залития в dev
+}
