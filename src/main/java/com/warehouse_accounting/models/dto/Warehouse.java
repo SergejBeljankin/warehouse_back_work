1c6f00f086a3baa1ff4a0324693ded017874df48
ismaylov Nazir <Gympills@gmail.com>
2021-02-01 19:16:23 +0500

    asd

--- src/main/java/com/warehouse_accounting/models/dto/Warehouse.java
+++ src/main/java/com/warehouse_accounting/models/dto/Warehouse.java
@@ -1,12 +1,14 @@
 package com.warehouse_accounting.models.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@Builder
 @Data
 @Table
 @NoArgsConstructor
