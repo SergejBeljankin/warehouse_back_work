394e3776e3cdf810ce35787166bb09e64b976707
VeraLimano <saydashevav@mail.ru>
2021-02-01 21:25:34 +0500

    modelTypeOfContractor

---
+++ src/main/java/com/warehouse_accounting/models/TypeOfContractor.java
@@ -0,0 +1,33 @@
+package com.warehouse_accounting.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "type_of_contractors")
+public class TypeOfContractor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String sortNumber;
+}
