bf0ee99e3a5ecd1cc70602c5de32be1899262590
Akromo <zloekrevedko@yandex.ru>
2021-02-01 13:27:21 +0300

    добавил javax.persistence

--- src/main/java/com/warehouse_accounting/models/TaxSystem.java
+++ src/main/java/com/warehouse_accounting/models/TaxSystem.java
@@ -1,11 +1,24 @@
 package com.warehouse_accounting.models;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class TaxSystem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "sortNumber")
     private String sortNumber;
 }
