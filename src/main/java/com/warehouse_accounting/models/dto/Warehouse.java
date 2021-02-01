5c939dd919e4ee558ddc3450ca329a18c4d54797
ismaylov Nazir <Gympills@gmail.com>
2021-02-01 13:24:05 +0500

    sfd

--- src/main/java/com/warehouse_accounting/models/dto/Warehouse.java
+++ src/main/java/com/warehouse_accounting/models/dto/Warehouse.java
@@ -1,15 +1,27 @@
 package com.warehouse_accounting.models.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Warehouse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "sortNumber")
     private String sortNumber;
+    @Column(name = "address")
     private String address;
+    @Column(name = "commentToAddress")
     private String commentToAddress;
+    @Column(name = "comment")
     private String comment;
 }
