174085279432f434e31627417aaf193ccf3b2724
Vitaliy Shapiro <shapirovet@gmail.com>
2021-02-01 16:14:15 +0300

    bug fix #1 add model Unit

--- src/main/java/com/warehouse_accounting/models/Unit.java
+++ src/main/java/com/warehouse_accounting/models/Unit.java
@@ -1,13 +1,36 @@
 package com.warehouse_accounting.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "units")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Unit {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Column()
     private Long id;
+
+    @Column()
     private String shortName;
+
+    @Column()
     private String fullName;
+
+    @Column()
     private String sortNumber;
 }
