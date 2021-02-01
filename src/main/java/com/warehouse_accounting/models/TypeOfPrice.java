3ae8a3c4cec19407fba1503771771dc02589ef8e
Dmitry <dtraider@mail.ru>
2021-02-01 13:42:20 +0300

    bug fix 2021.02.01

--- src/main/java/com/warehouse_accounting/models/TypeOfPrice.java
+++ src/main/java/com/warehouse_accounting/models/TypeOfPrice.java
@@ -1,13 +1,37 @@
 package com.warehouse_accounting.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GeneratorType;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "TypesOfPrice")
 public class TypeOfPrice {
+
+    @Id
+    @GeneratedValue()
+    @Column(name = "Id", unique = true)
     private Long id;
+
+    @Column(name = "Name")
     private String name;
+
+    @Column(name = "SortNumber")
     private String sortNumber;
 }
+
+
