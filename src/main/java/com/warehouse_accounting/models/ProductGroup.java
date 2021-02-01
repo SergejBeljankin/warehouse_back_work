f8395437a27a91305385e039c0f84ec24053e7bb
Gon4iyKot <tsapko.vladimir@gmail.com>
2021-02-01 15:07:54 +0400

    jpa annotations

--- src/main/java/com/warehouse_accounting/models/ProductGroup.java
+++ src/main/java/com/warehouse_accounting/models/ProductGroup.java
@@ -4,15 +4,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 @Data
+@Entity
+@Table(name = "product_group")
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String name;
 
+    @Column(name = "sort_number")
     private String sortNumber;
 
+    @ManyToOne
+    @JoinColumn(name = "parent_product_group_id")
     private ProductGroup productGroup;
 }
