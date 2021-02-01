dda5ccaf8f74c250ddb639a1a52ebcd1eeb71b87
User <anatienko.yv@gmail.com>
2021-02-01 18:09:14 +0300

    change model Position

--- src/main/java/com/warehouse_accounting/models/Position.java
+++ src/main/java/com/warehouse_accounting/models/Position.java
@@ -1,50 +1,31 @@
 package com.warehouse_accounting.models;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import java.util.Objects;
 
-@Getter
-@Setter
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "warehouse_position", indexes = @Index(name = "position_index", columnList = "id"))
+@Table(name = "positions")
 public class Position {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "position_gen")
-    @SequenceGenerator(name = "position_gen", sequenceName = "POSITION_GEN")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String name;
+
+    @Column
     private String sortNumber;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Position position = (Position) o;
-
-        if (!Objects.equals(id, position.id)) return false;
-        if (!Objects.equals(name, position.name)) return false;
-        return Objects.equals(sortNumber, position.sortNumber);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (sortNumber != null ? sortNumber.hashCode() : 0);
-        return result;
-    }
 }
