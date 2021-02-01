47137e07723a136fcc8570816a62f172436d7bdc
User <anatienko.yv@gmail.com>
2021-02-01 23:31:59 +0300

    PositionDTO add AllArgsConstructor

--- src/main/java/com/warehouse_accounting/models/dto/PositionDto.java
+++ src/main/java/com/warehouse_accounting/models/dto/PositionDto.java
@@ -1,10 +1,12 @@
 package com.warehouse_accounting.models.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
+@AllArgsConstructor
 public class PositionDto {
     Long id;
     String name;
