616b4bf6f34db9889755866910c48659ecedbbe3
Gon4iyKot <tsapko.vladimir@gmail.com>
2021-02-02 12:55:53 +0400

    created AttributeOfCalculationObject model

---
+++ src/main/java/com/warehouse_accounting/models/AttributeOfCalculationObject.java
@@ -0,0 +1,35 @@
+package com.warehouse_accounting.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "attributes_of_calculation_object")
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class AttributeOfCalculationObject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String sortNumber;
+
+    @Column
+    @Builder.Default
+    private Boolean isService = false;
+}
