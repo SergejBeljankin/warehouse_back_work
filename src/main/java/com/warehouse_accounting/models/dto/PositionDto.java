003e161e36b28f3af41b516937dcd0d10ba194ab
User <anatienko.yv@gmail.com>
2021-02-03 13:55:20 +0300

    change PositionDTO

--- src/main/java/com/warehouse_accounting/models/dto/PositionDto.java
+++ src/main/java/com/warehouse_accounting/models/dto/PositionDto.java
@@ -2,10 +2,12 @@ package com.warehouse_accounting.models.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Value
+@Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class PositionDto {
     Long id;
