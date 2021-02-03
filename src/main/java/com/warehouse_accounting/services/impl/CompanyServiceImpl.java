7df8adf4e1a63403ec83bdb40b2fe47c1af48177
Akromo <zloekrevedko@yandex.ru>
2021-02-03 21:11:41 +0300

    Перенес аннотацию @Service на CompanyServiceImpl

--- src/main/java/com/warehouse_accounting/services/impl/CompanyServiceImpl.java
+++ src/main/java/com/warehouse_accounting/services/impl/CompanyServiceImpl.java
@@ -1,15 +1,16 @@
 package com.warehouse_accounting.services.impl;
 
-import com.warehouse_accounting.models.Company;
 import com.warehouse_accounting.models.dto.CompanyDto;
 import com.warehouse_accounting.repositories.CompanyRepository;
 import com.warehouse_accounting.services.interfaces.CompanyService;
 import com.warehouse_accounting.util.ConverterDto;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Transactional
+@Service
 public class CompanyServiceImpl implements CompanyService {
 
     private final CompanyRepository compRep;
