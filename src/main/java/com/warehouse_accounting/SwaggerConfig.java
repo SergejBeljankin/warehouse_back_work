c5e85b3f1537adce750af7672585766103a9b4eb
GIgorV <gigor907@gmail.com>
2021-02-05 12:06:36 +0300

    added ApiInfoBuilder

--- src/main/java/com/warehouse_accounting/SwaggerConfig.java
+++ src/main/java/com/warehouse_accounting/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.warehouse_accounting;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -9,8 +10,6 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -31,11 +30,11 @@ public class SwaggerConfig {
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "REST API from JM",
-                "Trade and warehouse management.",
-                "Elementary",
-                "", contact,
-                "", "", Collections.emptyList());
+        return new ApiInfoBuilder()
+                .title("REST API from JM")
+                .description("Trade and warehouse management.")
+                .version("Elementary")
+                .contact(contact)
+                .build();
     }
 }
