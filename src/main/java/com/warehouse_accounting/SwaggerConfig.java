576cf6313cfcffdfce2834eb0c7249de34265d46
GIgorV <gigor907@gmail.com>
2021-02-05 11:28:04 +0300

    added SwaggerConfig and apiInfo

---
+++ src/main/java/com/warehouse_accounting/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.warehouse_accounting;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private final Contact contact = new Contact(
+            "", "", ""
+    );
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.warehouse_accounting.controllers"))
+                .paths(PathSelectors.regex("/api.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "REST API from JM",
+                "Trade and warehouse management.",
+                "Elementary",
+                "", contact,
+                "", "", Collections.emptyList());
+    }
+}
