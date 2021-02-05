7002e0e01f2f91cc4a040bed678066a3f090d052
Vitaliy Shapiro <shapirovet@gmail.com>
2021-02-05 18:42:20 +0300

    add util converterDto class in Service.

--- src/main/java/com/warehouse_accounting/services/impl/LegalDetailServiceImpl.java
+++ src/main/java/com/warehouse_accounting/services/impl/LegalDetailServiceImpl.java
@@ -1,9 +1,9 @@
 package com.warehouse_accounting.services.impl;
 
-import com.warehouse_accounting.models.LegalDetail;
 import com.warehouse_accounting.models.dto.LegalDetailDto;
 import com.warehouse_accounting.repositories.LegalDetailRepository;
 import com.warehouse_accounting.services.interfaces.LegalDetailService;
+import com.warehouse_accounting.util.ConverterDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,49 +31,16 @@ public class LegalDetailServiceImpl implements LegalDetailService {
 
     @Override
     public void create(LegalDetailDto legalDetailDto) {
-        legalDetailRepository.save(convertToModel(legalDetailDto));
+        legalDetailRepository.save(ConverterDto.convertToModel(legalDetailDto));
     }
 
     @Override
     public void update(LegalDetailDto legalDetailDto) {
-        legalDetailRepository.save(convertToModel(legalDetailDto));
+        legalDetailRepository.save(ConverterDto.convertToModel(legalDetailDto));
     }
 
     @Override
     public void deleteById(Long id) {
         legalDetailRepository.deleteById(id);
-    }
-
-    private LegalDetail convertToModel (LegalDetailDto legalDetailDto){
-        return LegalDetail.builder()
-                .id(legalDetailDto.getId())
-                .lastName(legalDetailDto.getLastName())
-                .firstName(legalDetailDto.getFirstName())
-                .middleName(legalDetailDto.getMiddleName())
-                .address(legalDetailDto.getAddress())
-                .commentToAddress(legalDetailDto.getCommentToAddress())
-                .inn(legalDetailDto.getInn())
-                .okpo(legalDetailDto.getOkpo())
-                .ogrnip(legalDetailDto.getOgrnip())
-                .numberOfTheCertificate(legalDetailDto.getNumberOfTheCertificate())
-                .dateOfTheCertificate(legalDetailDto.getDateOfTheCertificate())
-                .typeOfContractor(Util.convertToModel(legalDetailDto.getTypeOfContractorDto()))
-                .build();
-    }
-    private LegalDetailDto convertToDto(LegalDetail legalDetail){
-        return LegalDetailDto.builder()
-                .id(legalDetail.getId())
-                .lastName(legalDetail.getLastName())
-                .firstName(legalDetail.getFirstName())
-                .middleName(legalDetail.getMiddleName())
-                .address(legalDetail.getAddress())
-                .commentToAddress(legalDetail.getCommentToAddress())
-                .inn(legalDetail.getInn())
-                .okpo(legalDetail.getOkpo())
-                .ogrnip(legalDetail.getOgrnip())
-                .numberOfTheCertificate(legalDetail.getNumberOfTheCertificate())
-                .dateOfTheCertificate(legalDetail.getDateOfTheCertificate())
-                .typeOfContractorDto(Util.convertToDto(legalDetail.getTypeOfContractor()))
-                .build();
     }
 }
