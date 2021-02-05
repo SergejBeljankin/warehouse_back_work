a126ed7925214efd093d57790b78033f6193abd9
Sidorenkomv <sir.sidorenkomv@yandex.ru>
2021-02-05 12:53:57 +0000

    Update RoleRepository.java

--- src/main/java/com/warehouse_accounting/repositories/RoleRepository.java
+++ src/main/java/com/warehouse_accounting/repositories/RoleRepository.java
@@ -10,8 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface RoleRepository extends
-        JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
     @Query("SELECT new com.warehouse_accounting.models.dto.RoleDto(" +
             "r.id," +
             "r.name," +
