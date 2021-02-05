5f8c54fc04502ec66c468b72a236263041eefc66
Dmitry <dtraider@mail.ru>
2021-02-05 09:05:34 +0300

    bug fix

--- src/main/java/com/warehouse_accounting/repositories/UnitRepository.java
+++ src/main/java/com/warehouse_accounting/repositories/UnitRepository.java
@@ -3,7 +3,6 @@ package com.warehouse_accounting.repositories;
 import com.warehouse_accounting.models.Unit;
 import com.warehouse_accounting.models.dto.UnitDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -29,28 +28,5 @@ public interface UnitRepository extends JpaRepository<Unit, Long> {
             "u.sortNumber)" +
             "FROM Unit u WHERE u.id = :id")
     UnitDto getById(@Param("id") Long id);
-
-
-    @Modifying(clearAutomatically = true)
-    @Query(value = "INSERT INTO Units (short_name,full_name,sort_number) VALUES (" +
-            ":#{#unitDto.shortName}," +
-            ":#{#unitDto.fullName}," +
-            ":#{#unitDto.sortNumber})"
-            ,nativeQuery = true)
-    void create(@Param("unitDto") UnitDto unitDto);
-
-
-    @Modifying(clearAutomatically = true)
-    @Query("UPDATE Unit u SET " +
-            "u.shortName =:#{#unitDto.shortName}," +
-            "u.fullName=:#{#unitDto.fullName}," +
-            "u.sortNumber=:#{#unitDto.sortNumber} " +
-            "WHERE u.id = :#{#unitDto.id}")
-    void update(@Param("unitDto") UnitDto unitDto);
-
-
-    @Modifying(clearAutomatically = true)
-    @Query("DELETE Unit u WHERE u.id = :id")
-    void deleteById(@Param("id") Long id);
 
 }
--- src/main/java/com/warehouse_accounting/services/impl/UnitServiceImpl.java
+++ src/main/java/com/warehouse_accounting/services/impl/UnitServiceImpl.java
@@ -1,9 +1,9 @@
 package com.warehouse_accounting.services.impl;
 
+import com.warehouse_accounting.models.Unit;
 import com.warehouse_accounting.models.dto.UnitDto;
 import com.warehouse_accounting.repositories.UnitRepository;
 import com.warehouse_accounting.services.interfaces.UnitService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +13,8 @@ import java.util.List;
 @Transactional
 public class UnitServiceImpl implements UnitService {
 
-    private UnitRepository unitRepository;
+    private final UnitRepository unitRepository;
 
-    @Autowired
     public UnitServiceImpl(UnitRepository unitRepository) {
         this.unitRepository = unitRepository;
     }
@@ -32,17 +31,34 @@ public class UnitServiceImpl implements UnitService {
 
     @Override
     public void create(UnitDto unitDto) {
-        unitRepository.create(unitDto);
+        unitRepository.save(convertToModel(unitDto));
     }
 
     @Override
     public void update(UnitDto unitDto) {
-        unitRepository.update(unitDto);
+        unitRepository.save(convertToModel(unitDto));
     }
 
     @Override
     public void deleteById(Long id) {
         unitRepository.deleteById(id);
+    }
+
+    private Unit convertToModel (UnitDto unitDto){
+        return Unit.builder()
+                .id(unitDto.getId())
+                .shortName(unitDto.getShortName())
+                .fullName(unitDto.getFullName())
+                .sortNumber(unitDto.getSortNumber())
+                .build();
+    }
+    private UnitDto convertToDto(Unit unit){
+        return UnitDto.builder()
+                .id(unit.getId())
+                .shortName(unit.getShortName())
+                .fullName(unit.getFullName())
+                .sortNumber(unit.getSortNumber())
+                .build();
     }
 
 }
