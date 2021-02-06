0ed16616a59624626a5e59dc60c3aeea11a7ea33
Gon4iyKot <tsapko.vladimir@gmail.com>
2021-02-06 18:19:45 +0400

    transferred converters to ConverterDto

--- src/main/java/com/warehouse_accounting/services/impl/PositionServiceImpl.java
+++ src/main/java/com/warehouse_accounting/services/impl/PositionServiceImpl.java
@@ -1,6 +1,5 @@
 package com.warehouse_accounting.services.impl;
 
-import com.warehouse_accounting.models.Position;
 import com.warehouse_accounting.models.dto.PositionDto;
 import com.warehouse_accounting.repositories.PositionRepository;
 import com.warehouse_accounting.services.interfaces.PositionService;
@@ -8,6 +7,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static com.warehouse_accounting.util.ConverterDto.convertToModel;
 
 @Service
 @Transactional
@@ -42,23 +43,5 @@ public class PositionServiceImpl implements PositionService {
     @Override
     public void deleteById(Long id) {
         positionRepository.deleteById(id);
-    }
-
-    //TODO перенести в класс конвертера, когда появится
-    public static Position convertToModel(PositionDto positionDto) {
-        return Position.builder()
-                .id(positionDto.getId())
-                .name(positionDto.getName())
-                .sortNumber(positionDto.getSortNumber())
-                .build();
-    }
-
-    //TODO перенести в класс конвертера, когда появится
-    public static PositionDto convertToDto(Position position) {
-        return PositionDto.builder()
-                .id(position.getId())
-                .name(position.getName())
-                .sortNumber(position.getSortNumber())
-                .build();
     }
 }
--- src/main/java/com/warehouse_accounting/util/ConverterDto.java
+++ src/main/java/com/warehouse_accounting/util/ConverterDto.java
@@ -1,6 +1,8 @@
 package com.warehouse_accounting.util;
 
+import com.warehouse_accounting.models.Position;
 import com.warehouse_accounting.models.Role;
+import com.warehouse_accounting.models.dto.PositionDto;
 import com.warehouse_accounting.models.dto.RoleDto;
 
 
@@ -22,6 +24,22 @@ public class ConverterDto {
                 .id(roleDto.getId())
                 .name(roleDto.getName())
                 .sortNumber(roleDto.getSortNumber())
+                .build();
+    }
+
+    public static Position convertToModel(PositionDto positionDto) {
+        return Position.builder()
+                .id(positionDto.getId())
+                .name(positionDto.getName())
+                .sortNumber(positionDto.getSortNumber())
+                .build();
+    }
+
+    public static PositionDto convertToDto(Position position) {
+        return PositionDto.builder()
+                .id(position.getId())
+                .name(position.getName())
+                .sortNumber(position.getSortNumber())
                 .build();
     }
 }
