ee6a416399c21887f4e0e6d7c0c92e6e868f6377
Dmitry <dtraider@mail.ru>
2021-02-06 12:07:12 +0300

    add changes

--- src/main/java/com/warehouse_accounting/util/ConverterDto.java
+++ src/main/java/com/warehouse_accounting/util/ConverterDto.java
@@ -2,10 +2,12 @@ package com.warehouse_accounting.util;
 
 import com.warehouse_accounting.models.BankAccount;
 import com.warehouse_accounting.models.Role;
+import com.warehouse_accounting.models.TaxSystem;
 import com.warehouse_accounting.models.Unit;
 import com.warehouse_accounting.models.Warehouse;
 import com.warehouse_accounting.models.dto.BankAccountDto;
 import com.warehouse_accounting.models.dto.RoleDto;
+import com.warehouse_accounting.models.dto.TaxSystemDto;
 import com.warehouse_accounting.models.dto.UnitDto;
 import com.warehouse_accounting.models.dto.WarehouseDto;
 
@@ -15,19 +17,19 @@ public class ConverterDto {
     private ConverterDto() {
     }
 
-    public static RoleDto convertToDto(Role role) {
-        return RoleDto.builder()
-                .id(role.getId())
-                .name(role.getName())
-                .sortNumber(role.getSortNumber())
-                .build();
-    }
-
     public static Role convertToModel(RoleDto roleDto) {
         return Role.builder()
                 .id(roleDto.getId())
                 .name(roleDto.getName())
                 .sortNumber(roleDto.getSortNumber())
+                .build();
+    }
+
+    public static RoleDto convertToDto(Role role) {
+        return RoleDto.builder()
+                .id(role.getId())
+                .name(role.getName())
+                .sortNumber(role.getSortNumber())
                 .build();
     }
 
@@ -74,6 +76,7 @@ public class ConverterDto {
                 .sortNumber(unit.getSortNumber())
                 .build();
     }
+
     public static Warehouse convertToModel(WarehouseDto warehouseDto) {
         return Warehouse.builder()
                 .id(warehouseDto.getId())
@@ -85,4 +88,29 @@ public class ConverterDto {
                 .build();
     }
 
+    public static WarehouseDto convertToDto(Warehouse warehouse) {
+        return WarehouseDto.builder()
+                .id(warehouse.getId())
+                .name(warehouse.getName())
+                .sortNumber(warehouse.getSortNumber())
+                .address(warehouse.getAddress())
+                .commentToAddress(warehouse.getCommentToAddress())
+                .comment(warehouse.getComment())
+                .build();
+    }
+
+    public static TaxSystem convertToModel(TaxSystemDto taxSystemDto) {
+        return TaxSystem.builder()
+                .id(taxSystemDto.getId())
+                .name(taxSystemDto.getName())
+                .sortNumber(taxSystemDto.getSortNumber())
+                .build();
+    }
+    public static TaxSystemDto convertToDto(TaxSystem taxSystem) {
+        return TaxSystemDto.builder()
+                .id(taxSystem.getId())
+                .name(taxSystem.getName())
+                .sortNumber(taxSystem.getSortNumber())
+                .build();
+    }
 }
