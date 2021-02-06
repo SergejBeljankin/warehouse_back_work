afb6a6814ab3695d70367efe092c8df2a8a2d234
Dima854 <dimonn104@gmail.com>
2021-02-06 21:01:04 +0500

    Создал репозиторий и сервис для Image

--- src/main/java/com/warehouse_accounting/util/ConverterDto.java
+++ src/main/java/com/warehouse_accounting/util/ConverterDto.java
@@ -1,12 +1,29 @@
 package com.warehouse_accounting.util;
 
+import com.warehouse_accounting.models.Image;
 import com.warehouse_accounting.models.Role;
+import com.warehouse_accounting.models.dto.ImageDto;
 import com.warehouse_accounting.models.dto.RoleDto;
 
 
 public class ConverterDto {
 
     private ConverterDto() {
+    }
+
+    public static Image convertToModel (ImageDto imageDto){
+        return Image.builder()
+                .id(imageDto.getId())
+                .imageUrl(imageDto.getImageUrl())
+                .sortNumber(imageDto.getSortNumber())
+                .build();
+    }
+    public static ImageDto convertToDto(Image image){
+        return ImageDto.builder()
+                .id(image.getId())
+                .imageUrl(image.getImageUrl())
+                .sortNumber(image.getSortNumber())
+                .build();
     }
 
     public static RoleDto convertToDto(Role role) {
@@ -24,4 +41,7 @@ public class ConverterDto {
                 .sortNumber(roleDto.getSortNumber())
                 .build();
     }
+
+
+
 }
