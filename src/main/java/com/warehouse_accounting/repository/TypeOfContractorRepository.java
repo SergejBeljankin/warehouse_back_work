3b964ec79fed96456942c01ce1e69658a24041bf
ismaylov Nazir <Gympills@gmail.com>
2021-02-08 04:38:06 +0500

    repo

--- src/main/java/com/warehouse_accounting/repository/TypeOfContractorRepository.java
+++ src/main/java/com/warehouse_accounting/repository/TypeOfContractorRepository.java
@@ -1,13 +1,15 @@
 package com.warehouse_accounting.repository;
 
+import com.warehouse_accounting.models.TypeOfContractor;
 import com.warehouse_accounting.models.dto.TypeOfContractorDto;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public interface TypeOfContractorRepository {
+public interface TypeOfContractorRepository extends JpaRepository<TypeOfContractor,Long> {
     @Query("SELECT new com.warehouse_accounting.models.dto.TypeOfContractorDto(" +
             "tc.id," +
             "tc.name," +
