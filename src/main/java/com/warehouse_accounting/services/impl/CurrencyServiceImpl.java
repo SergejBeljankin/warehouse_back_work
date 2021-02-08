75a8bed64d948ad72269638027a19f4eab3ee388
Sidorenkomv <sir.sidorenkomv@yandex.ru>
2021-02-08 12:13:11 +0000

    Update CurrencyServiceImpl.java

--- src/main/java/com/warehouse_accounting/services/impl/CurrencyServiceImpl.java
+++ src/main/java/com/warehouse_accounting/services/impl/CurrencyServiceImpl.java
@@ -9,8 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Transactional
 @Service
+@Transactional
 public class CurrencyServiceImpl implements CurrencyService {
 
     private final CurrencyRepository currencyRepository;
