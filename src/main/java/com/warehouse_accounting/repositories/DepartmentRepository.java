6f91f294c0300a1240c1a5f31bac4c23e3b58418
Ivan <@>
2021-02-08 22:41:27 +0300

    add Department Repositiry and Service 2

--- src/main/java/com/warehouse_accounting/repositories/DepartmentRepository.java
+++ src/main/java/com/warehouse_accounting/repositories/DepartmentRepository.java
@@ -5,9 +5,11 @@ import com.warehouse_accounting.models.dto.DepartmentDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository <Department, Long> {
     @Query("SELECT NEW com.warehouse_accounting.models.dto.DepartmentDto(" +
             "d.id," +
