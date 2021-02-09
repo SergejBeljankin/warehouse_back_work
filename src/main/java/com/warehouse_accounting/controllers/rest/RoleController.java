493979bd590fe2cb136c43580014263b7334c7dc
Dima854 <dimonn104@gmail.com>
2021-02-10 00:16:14 +0500

    Поправил

--- src/main/java/com/warehouse_accounting/controllers/rest/RoleController.java
+++ src/main/java/com/warehouse_accounting/controllers/rest/RoleController.java
@@ -7,7 +7,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
