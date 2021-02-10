446013b54c19e8717511b728cfd3fced4a00843f
ismaylov Nazir <Gympills@gmail.com>
2021-02-10 17:24:37 +0500

    repo

--- src/main/java/com/warehouse_accounting/controllers/TypeOfContractor_Rest_Controller.java
+++
@@ -1,4 +0,0 @@
-package com.warehouse_accounting.controllers;
-
-public class TypeOfContractor_Rest_Controller {
-}
---
+++ src/main/java/com/warehouse_accounting/controllers/rest/TypeOfContractor_Rest_Controller.java
@@ -0,0 +1,13 @@
+package com.warehouse_accounting.controllers.rest;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Api
+@RequestMapping("/api/type_of_contractor")
+public class TypeOfContractor_Rest_Controller {
+
+
+
+
+}
