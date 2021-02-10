a9c48a293aa2e00b04834c15691be6d0953d1219
Akromo <zloekrevedko@yandex.ru>
2021-02-11 01:36:47 +0300

    + @Tag

--- src/main/java/com/warehouse_accounting/controllers/rest/TaxSystemController.java
+++ src/main/java/com/warehouse_accounting/controllers/rest/TaxSystemController.java
@@ -18,9 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Tag(name = "TaxSystem Rest Controller", description = "CRUD операции с объектами")
+
 @RestController
 @RequestMapping("/api/tax_systems")
+@Tag(name = "TaxSystem Rest Controller", description = "CRUD операции с объектами")
 public class TaxSystemController {
     private final TaxSystemService taxSystemService;
 
@@ -29,6 +30,7 @@ public class TaxSystemController {
     }
 
     @GetMapping
+    @Tag(name = "TaxSystem Rest Controller")
     @ApiOperation(value = "Возвращает все системы налогообложения", notes = "Возвращает список TaxSystemDto",
             response = TaxSystemDto.class, responseContainer = "List")
     @ApiResponses(value = {
@@ -42,6 +44,7 @@ public class TaxSystemController {
     }
 
     @GetMapping("/{id}")
+    @Tag(name = "TaxSystem Rest Controller")
     @ApiOperation(value = "Возвращает систему налогообложения с выбранным id", notes = "Возвращает TaxSystemDto",
             response = TaxSystemDto.class)
     @ApiResponses(value = {
@@ -50,49 +53,49 @@ public class TaxSystemController {
             @ApiResponse(code = 404, message = "Данный контролер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public TaxSystemDto getById(@ApiParam(name = "id", value = "Id of TaxSystemDto", required = true)
+    public TaxSystemDto getById(@ApiParam(name = "id", value = "Id нужного TaxSystemDto", required = true)
                                 @PathVariable("id") Long id) {
         return taxSystemService.getById(id);
     }
 
     @PutMapping
-    @ApiOperation(value = "Обновляет переданную систему налогообложения", notes = "Ничего не возвращает",
-            response = TaxSystemDto.class)
+    @Tag(name = "TaxSystem Rest Controller")
+    @ApiOperation(value = "Обновляет переданную систему налогообложения")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Успешное обновление системы налогообложения",
                     response = TaxSystemDto.class),
             @ApiResponse(code = 404, message = "Данный контролер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public void update(@ApiParam(name = "TaxSystemDto", value = "object of TaxSystemDto for update", required = true)
+    public void update(@ApiParam(name = "TaxSystemDto", value = "объект TaxSystemDto для обновления", required = true)
                        @RequestBody TaxSystemDto taxSystemDto) {
         taxSystemService.update(taxSystemDto);
     }
 
     @PostMapping
-    @ApiOperation(value = "Создает переданную систему налогообложения", notes = "Ничего не возвращает",
-            response = TaxSystemDto.class)
+    @Tag(name = "TaxSystem Rest Controller")
+    @ApiOperation(value = "Создает переданную систему налогообложения")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Успешное создание системы налогообложения",
                     response = TaxSystemDto.class),
             @ApiResponse(code = 404, message = "Данный контролер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public void create(@ApiParam(name = "TaxSystemDto", value = "object of TaxSystemDto for create", required = true)
+    public void create(@ApiParam(name = "TaxSystemDto", value = "объект TaxSystemDto для создания", required = true)
                        @RequestBody TaxSystemDto taxSystemDto) {
         taxSystemService.create(taxSystemDto);
     }
 
     @DeleteMapping("/{id}")
-    @ApiOperation(value = "Удаляет систему налогообложения с выбранным id",
-            notes = "Ничего не возвращает", response = TaxSystemDto.class)
+    @Tag(name = "TaxSystem Rest Controller")
+    @ApiOperation(value = "Удаляет систему налогообложения с выбранным id")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Успешное удаление системы налогообложения",
                     response = TaxSystemDto.class),
             @ApiResponse(code = 404, message = "Данный контролер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public void deleteById(@ApiParam(name = "id", value = "Id of TaxSystemDto for delete", required = true)
+    public void deleteById(@ApiParam(name = "id", value = "Id TaxSystemDto для удаления", required = true)
                            @PathVariable("id") Long id) {
         taxSystemService.deleteById(id);
     }
