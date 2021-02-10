9dfece3c7cd843662662a2e5602220b164324454
Gon4iyKot <tsapko.vladimir@gmail.com>
2021-02-10 12:27:05 +0400

    checkEntityService added

---
+++ src/main/java/com/warehouse_accounting/util/exception/NotFoundEntityException.java
@@ -0,0 +1,8 @@
+package com.warehouse_accounting.util.exception;
+
+public class NotFoundEntityException extends RuntimeException{
+
+    public NotFoundEntityException(String message) {
+        super(message);
+    }
+}
