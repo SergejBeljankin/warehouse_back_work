9ea210a32ee5d6116b3e1a5f725d90cd39814986
Vitaliy Shapiro <shapirovet@gmail.com>
2021-02-10 22:00:37 +0300

    add model Contractor

---
+++ src/main/java/com/warehouse_accounting/models/Contractor.java
@@ -0,0 +1,73 @@
+package com.warehouse_accounting.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "contractors")
+public class Contractor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(unique = true)
+    private String inn;
+
+    @Column
+    private String sortNumber;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String fax;
+
+    @Column
+    private String email;
+
+    @Column
+    private String address;
+
+    @Column
+    private String commentToAddress;
+
+    @Column
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ContractorGroup contractorGroup;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private TypeOfContractor typeOfContractor;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private TypeOfPrice typeOfPrice;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<BankAccount> bankAccounts;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private LegalDetail legalDetail;
+}
