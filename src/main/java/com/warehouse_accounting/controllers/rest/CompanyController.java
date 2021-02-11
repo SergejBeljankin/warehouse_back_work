690c8eb14830016cc2a8ef4c7d43fe1943394a81
Gon4iyKot <tsapko.vladimir@gmail.com>
2021-02-11 23:25:54 +0400

    added swagger annotations

--- src/main/java/com/warehouse_accounting/controllers/rest/CompanyController.java
+++ src/main/java/com/warehouse_accounting/controllers/rest/CompanyController.java
@@ -3,26 +3,93 @@ package com.warehouse_accounting.controllers.rest;
 import com.warehouse_accounting.models.dto.CompanyDto;
 import com.warehouse_accounting.services.CheckEntityService;
 import com.warehouse_accounting.services.impl.CompanyServiceImpl;
+import com.warehouse_accounting.util.ConverterDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/companies")
+@Tag(name = "Company RESTController", description = "controller for doing some CRUD-magic with companies")
 public class CompanyController {
 
-    @Autowired
-    private CompanyServiceImpl companyService;
+    private final CompanyServiceImpl companyService;
 
-    @Autowired
-    private CheckEntityService checkEntityService;
+    public CompanyController(CompanyServiceImpl companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping
+    @ApiOperation(value = "getAll", notes = "Получение списка всех Company")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное получение списка Company"),
+            @ApiResponse(responseCode = "404", description = "Данный контроллер не найден"),
+            @ApiResponse(responseCode = "403", description = "Операция запрещена"),
+            @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
+    )
+    public List<CompanyDto> getAll() {
+        return companyService.getAll();
+    }
 
     @GetMapping("/{id}")
-    public CompanyDto getById(@PathVariable long id) {
-        checkEntityService.checkExistUnitById(id);
+    @ApiOperation(value = "getById", notes = "Получение Company по ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное получение Company"),
+            @ApiResponse(responseCode = "404", description = "Данный контроллер не найден"),
+            @ApiResponse(responseCode = "403", description = "Операция запрещена"),
+            @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
+    )
+    public CompanyDto getById(@PathVariable("id") long id) {
         return companyService.getById(id);
+    }
+
+    @PostMapping
+    @ApiOperation(value = "create", notes = "Сохранение Company")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное сохранение Company"),
+            @ApiResponse(responseCode = "404", description = "Данный контроллер не найден"),
+            @ApiResponse(responseCode = "403", description = "Операция запрещена"),
+            @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
+    )
+    public void create(@RequestBody CompanyDto companyDto) {
+        companyService.create(companyDto);
+    }
+
+    @PutMapping
+    @ApiOperation(value = "update", notes = "Изменение Company")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное изменение Company"),
+            @ApiResponse(responseCode = "404", description = "Данный контроллер не найден"),
+            @ApiResponse(responseCode = "403", description = "Операция запрещена"),
+            @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
+    )
+    public void update(@RequestBody CompanyDto companyDto) {
+        companyService.update(companyDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "deleteById", notes = "Удаление Company")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное удаление Company"),
+            @ApiResponse(responseCode = "404", description = "Данный контроллер не найден"),
+            @ApiResponse(responseCode = "403", description = "Операция запрещена"),
+            @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
+    )
+    public void deleteById(@PathVariable("id") Long id) {
+        companyService.deleteById(id);
     }
 
 }
--- src/test/java/com/warehouse_accounting/WarehouseAccountingApplicationTests.java
+++ src/test/java/com/warehouse_accounting/WarehouseAccountingApplicationTests.java
@@ -12,64 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WarehouseAccountingApplicationTests {
 
-    @Autowired
-    private LegalDetailServiceImpl service;
-
-    @Autowired
-    private TypeOfContractorServiceImpl typeOfContractorService;
-
-    TypeOfContractorDto contractor1 = TypeOfContractorDto.builder()
-            .id((long)1)
-            .sortNumber("sin")
-            .name("sin").build();
-    TypeOfContractorDto contractor2 = TypeOfContractorDto.builder()
-            .id((long)2)
-            .sortNumber("sobaki")
-            .name("sobaki").build();
-
     @Test
     void contextLoads() {
-/*
-        typeOfContractorService.create(contractor1);
-        typeOfContractorService.create(contractor2);
-*/
     }
-
-    @Test
-    void create() {
-        LegalDetailDto legalDetailDto = LegalDetailDto.builder()
-                .firstName("sdfsdf")
-                .address("sdfdfsdfddddddddddddddd")
-                .typeOfContractorDto(contractor1).build();
-        service.create(legalDetailDto);
-    }
-
-    @Test
-    void update() {
-        LegalDetailDto legalDetailDto = LegalDetailDto.builder()
-                .id((long)2)
-                .firstName("lol")
-                .address("kek")
-                .typeOfContractorDto(contractor2).build();
-        service.update(legalDetailDto);
-    }
-
-    @Test
-    void getAll() {
-        for(LegalDetailDto legalDetailDto : service.getAll()) {
-            System.out.println(legalDetailDto);
-        }
-    }
-
-    @Test
-    void getOne() {
-        System.out.println(service.getById((long)1));
-    }
-
-    @Test
-    void delete() {
-        service.deleteById((long)2);
-    }
-
-
 }
