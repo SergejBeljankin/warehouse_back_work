c4cbe0d8c863389b8d7687d708ed529aba09504a
Gon4iyKot <tsapko.vladimir@gmail.com>
2021-02-11 23:28:47 +0400

    some cleaning

--- src/test/java/com/warehouse_accounting/WarehouseAccountingApplicationTests.java
+++ src/test/java/com/warehouse_accounting/WarehouseAccountingApplicationTests.java
@@ -1,12 +1,6 @@
 package com.warehouse_accounting;
 
-import com.warehouse_accounting.models.TypeOfContractor;
-import com.warehouse_accounting.models.dto.LegalDetailDto;
-import com.warehouse_accounting.models.dto.TypeOfContractorDto;
-import com.warehouse_accounting.services.impl.LegalDetailServiceImpl;
-import com.warehouse_accounting.services.impl.TypeOfContractorServiceImpl;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -15,4 +9,5 @@ class WarehouseAccountingApplicationTests {
     @Test
     void contextLoads() {
     }
+
 }
