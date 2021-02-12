b3f155342e4ca7809f4ecd500519ca2ff3e03739
Akromo <zloekrevedko@yandex.ru>
2021-02-12 17:28:33 +0300

    + ResponseEntity @Api(tags = "Image Rest Controller") - @Tag(name = "Image Rest Controller")

--- src/main/java/com/warehouse_accounting/controllers/rest/ImageController.java
+++ src/main/java/com/warehouse_accounting/controllers/rest/ImageController.java
@@ -1,11 +1,13 @@
 package com.warehouse_accounting.controllers.rest;
 
 import com.warehouse_accounting.models.dto.ImageDto;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +21,7 @@ import com.warehouse_accounting.services.interfaces.ImageService;
 import java.util.List;
 
 @RestController
+@Api(tags = "Image Rest Controller")
 @Tag(name = "Image Rest Controller", description = "CRUD операции с объектами")
 @RequestMapping("/api/images")
 public class ImageController {
@@ -29,7 +32,6 @@ public class ImageController {
     }
 
     @GetMapping
-    @Tag(name = "Image Rest Controller")
     @ApiOperation(value = "Возвращает все изображения", notes = "Возвращает список ImageDto",
             response = ImageDto.class, responseContainer = "List")
     @ApiResponses(value = {
@@ -37,12 +39,11 @@ public class ImageController {
             @ApiResponse(code = 404, message = "Данный контролер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public List<ImageDto> getAll() {
-        return imageService.getAll();
+    public ResponseEntity<List<ImageDto>> getAll() {
+        return ResponseEntity.ok(imageService.getAll());
     }
 
     @GetMapping("/{id}")
-    @Tag(name = "Image Rest Controller")
     @ApiOperation(value = "Возвращает изображение с выбранным id", notes = "Возвращает ImageDto",
             response = ImageDto.class)
     @ApiResponses(value = {
@@ -50,26 +51,25 @@ public class ImageController {
             @ApiResponse(code = 404, message = "Данный контролер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public ImageDto getById(@ApiParam(name = "id", value = "Id нужного ImageDto", required = true)
+    public ResponseEntity<ImageDto> getById(@ApiParam(name = "id", value = "Id нужного ImageDto", required = true)
                             @PathVariable("id") Long id) {
-        return imageService.getById(id);
+        return ResponseEntity.ok(imageService.getById(id));
     }
 
     @PutMapping
-    @Tag(name = "Image Rest Controller")
     @ApiOperation(value = "Обновляет переданное изображение")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Успешное обновление ImageDto"),
             @ApiResponse(code = 404, message = "Данный контролер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public void update(@ApiParam(name = "ImageDto", value = "Объект ImageDto для обновления", required = true)
+    public ResponseEntity<?> update(@ApiParam(name = "ImageDto", value = "Объект ImageDto для обновления", required = true)
                        @RequestBody ImageDto imageDto) {
         imageService.update(imageDto);
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping
-    @Tag(name = "Image Rest Controller")
     @ApiOperation(value = "Создает переданное изображение")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Успешное создание ImageDto",
@@ -77,21 +77,22 @@ public class ImageController {
             @ApiResponse(code = 404, message = "Данный контролер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public void create(@ApiParam(name = "ImageDto", value = "Объект ImageDto для создания", required = true)
+    public ResponseEntity<?> create(@ApiParam(name = "ImageDto", value = "Объект ImageDto для создания", required = true)
                        @RequestBody ImageDto imageDto) {
         imageService.create(imageDto);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
-    @Tag(name = "Image Rest Controller")
     @ApiOperation(value = "Удаляет изображение с выбранным id")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Успешное удаление ImageDto"),
             @ApiResponse(code = 404, message = "Данный контролер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public void deleteById(@ApiParam(name = "id", value = "Id ImageDto для удаления", required = true)
+    public ResponseEntity<?> deleteById(@ApiParam(name = "id", value = "Id ImageDto для удаления", required = true)
                            @PathVariable("id") Long id) {
         imageService.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
