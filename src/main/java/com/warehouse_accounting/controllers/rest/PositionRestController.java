f282532d4afe38e0ad7ed6b4cc481212b45f9357
VeraLimano <saydashevav@mail.ru>
2021-02-13 15:34:42 +0500

    done

--- src/main/java/com/warehouse_accounting/controllers/rest/PositionRestController.java
+++ src/main/java/com/warehouse_accounting/controllers/rest/PositionRestController.java
@@ -1,16 +1,21 @@
 package com.warehouse_accounting.controllers.rest;
 
 import com.warehouse_accounting.models.dto.PositionDto;
+import com.warehouse_accounting.models.dto.TaxSystemDto;
 import com.warehouse_accounting.services.interfaces.PositionService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,75 +23,75 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/positions")
-@Tag(name = "Position Rest Controller", description = "CRUD операции с объектами Position")
-public class PositionController {
+@Api(tags = "Position Rest Controller")
+@Tag(name = "Position Rest Controller", description = "CRUD операции с объектами")
+public class PositionRestController {
 
     private final PositionService service;
 
-    public PositionController(PositionService service) {
+    public PositionRestController(PositionService service) {
         this.service = service;
     }
 
     @GetMapping
-    @Tag(name = "Position Rest Controller")
-    @ApiOperation(value = "getAll", notes = "Получение списка всех Position")
+    @ApiOperation(value = "Возвращает список PositionDto")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Успешное получение списка Position"),
+            @ApiResponse(code = 200, message = "Успешное получение списка PositionDto"),
             @ApiResponse(code = 404, message = "Данный контроллер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public List<PositionDto> getAll() {
-        return service.getAll();
+    public ResponseEntity<List<PositionDto>> getAll() {
+        return ResponseEntity.ok(service.getAll());
     }
 
     @GetMapping("/{id}")
-    @Tag(name = "Position Rest Controller")
-    @ApiOperation(value = "getById", notes = "Получение Position")
+    @ApiOperation(value = "Возвращает PositionDto по id")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Успешное получение Position"),
+            @ApiResponse(code = 200, message = "Успешное получение PositionDto"),
             @ApiResponse(code = 404, message = "Данный контроллер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public PositionDto getById(@PathVariable Long id) {
-        return service.getById(id);
+    public ResponseEntity<PositionDto> getById(@ApiParam(name = "id", value = "id для получения PositionDto", required = true)
+                                               @PathVariable("id") Long id) {
+        return ResponseEntity.ok(service.getById(id));
     }
 
     @PostMapping
-    @Tag(name = "Position Rest Controller")
-    @ApiOperation(value = "create", notes = "Создание Position")
+    @ApiOperation(value = "Создает PositionDto")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Успешное создание Position"),
+            @ApiResponse(code = 200, message = "Успешное создание PositionDto"),
             @ApiResponse(code = 404, message = "Данный контроллер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public void create(PositionDto dto) {
-        service.create(dto);
+    public ResponseEntity<?> create(@ApiParam(name = "PositionDto", value = "объект PositionDto для создания",
+            required = true) @RequestBody PositionDto positionDto) {
+        service.create(positionDto);
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping
-    @Tag(name = "Position Rest Controller")
-    @ApiOperation(value = "update", notes = "Изменение Position")
+    @ApiOperation(value = "Обновляет PositionDto")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Успешное изменение Position"),
+            @ApiResponse(code = 200, message = "Успешное обновление PositionDto"),
             @ApiResponse(code = 404, message = "Данный контроллер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public void update(PositionDto dto) {
-        service.update(dto);
+    public ResponseEntity<?> update(@ApiParam(name = "PositionDto", value = "объект PositionDto для обновления",
+            required = true) @RequestBody PositionDto positionDto) {
+        service.update(positionDto);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
-    @Tag(name = "Position Rest Controller")
-    @ApiOperation(value = "deleteById", notes = "Удаление Position")
+    @ApiOperation(value = "Удаляет PositionDto по id")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Успешное удаление Position"),
+            @ApiResponse(code = 200, message = "Успешное удаление PositionDto"),
             @ApiResponse(code = 404, message = "Данный контроллер не найден"),
             @ApiResponse(code = 403, message = "Операция запрещена"),
             @ApiResponse(code = 401, message = "Нет доступа к данной операции")})
-    public void deleteById(@PathVariable Long id) {
+    public ResponseEntity<?> deleteById(@ApiParam(name = "id", value = "id для удаления PositionDto",
+            required = true) @PathVariable("id") Long id) {
         service.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
-
-
-
