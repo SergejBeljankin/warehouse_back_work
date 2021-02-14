f0199da3bb046cdc4cb3b592e350d458ed18104c
Gon4iyKot <tsapko.vladimir@gmail.com>
2021-02-14 16:11:07 +0400

    Tags fixed + return values

--- src/main/java/com/warehouse_accounting/controllers/rest/CompanyController.java
+++ src/main/java/com/warehouse_accounting/controllers/rest/CompanyController.java
@@ -1,15 +1,14 @@
 package com.warehouse_accounting.controllers.rest;
 
 import com.warehouse_accounting.models.dto.CompanyDto;
-import com.warehouse_accounting.services.CheckEntityService;
 import com.warehouse_accounting.services.impl.CompanyServiceImpl;
-import com.warehouse_accounting.util.ConverterDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +22,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/companies")
+@Api(tags = "Company RESTController")
 @Tag(name = "Company RESTController", description = "controller for doing some CRUD-magic with companies")
 public class CompanyController {
 
@@ -33,7 +33,6 @@ public class CompanyController {
     }
 
     @GetMapping
-    @Tag(name = "Company RESTController")
     @ApiOperation(value = "getAll", notes = "Получение списка всех Company")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Успешное получение списка Company"),
@@ -41,12 +40,11 @@ public class CompanyController {
             @ApiResponse(responseCode = "403", description = "Операция запрещена"),
             @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
     )
-    public List<CompanyDto> getAll() {
-        return companyService.getAll();
+    public ResponseEntity<List<CompanyDto>> getAll() {
+        return ResponseEntity.ok(companyService.getAll());
     }
 
     @GetMapping("/{id}")
-    @Tag(name = "Company RESTController")
     @ApiOperation(value = "getById", notes = "Получение Company по ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Успешное получение Company"),
@@ -54,12 +52,11 @@ public class CompanyController {
             @ApiResponse(responseCode = "403", description = "Операция запрещена"),
             @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
     )
-    public CompanyDto getById(@PathVariable("id") long id) {
-        return companyService.getById(id);
+    public ResponseEntity<CompanyDto> getById(@PathVariable("id") long id) {
+        return ResponseEntity.ok(companyService.getById(id));
     }
 
     @PostMapping
-    @Tag(name = "Company RESTController")
     @ApiOperation(value = "create", notes = "Сохранение Company")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Успешное сохранение Company"),
@@ -67,12 +64,12 @@ public class CompanyController {
             @ApiResponse(responseCode = "403", description = "Операция запрещена"),
             @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
     )
-    public void create(@RequestBody CompanyDto companyDto) {
+    public ResponseEntity<?> create(@RequestBody CompanyDto companyDto) {
         companyService.create(companyDto);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping
-    @Tag(name = "Company RESTController")
     @ApiOperation(value = "update", notes = "Изменение Company")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Успешное изменение Company"),
@@ -80,12 +77,12 @@ public class CompanyController {
             @ApiResponse(responseCode = "403", description = "Операция запрещена"),
             @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
     )
-    public void update(@RequestBody CompanyDto companyDto) {
+    public ResponseEntity<?> update(@RequestBody CompanyDto companyDto) {
         companyService.update(companyDto);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    @Tag(name = "Company RESTController")
     @ApiOperation(value = "deleteById", notes = "Удаление Company")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Успешное удаление Company"),
@@ -93,8 +90,8 @@ public class CompanyController {
             @ApiResponse(responseCode = "403", description = "Операция запрещена"),
             @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
     )
-    public void deleteById(@PathVariable("id") Long id) {
+    public ResponseEntity<?> deleteById(@PathVariable("id") Long id) {
         companyService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
-
 }
