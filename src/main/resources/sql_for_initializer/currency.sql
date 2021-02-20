a3e95e056c6eeb78c1dcfdeeac4412d9a88eedd2
Dmitry <dtraider@mail.ru>
2021-02-20 11:21:48 +0300

    init BD Currency

--- src/main/resources/sql_for_initializer/currency.sql
+++ src/main/resources/sql_for_initializer/currency.sql
@@ -1,6 +1,5 @@
 INSERT INTO currencies (short_name, full_name, digital_code, letter_code, sort_number)
-                       VALUES ('RUB', 'Russian Ruble', '643', '₽', '1');
+                       VALUES ('РУБ', 'Рубль', '643', '₽', '1');
 INSERT INTO currencies (short_name, full_name, digital_code, letter_code, sort_number)
-                       VALUES ('USD', 'US Dollar', '840', '$', '2');
-INSERT INTO currencies (short_name, full_name, digital_code, letter_code, sort_number)
-                       VALUES ('EUR', 'EURO', '978', '€', '3');
+                       VALUES ('ДОЛ', 'Доллар', '840', '$', '2');
+
