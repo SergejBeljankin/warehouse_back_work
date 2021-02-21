5d7c9af5ea592fc2c0e794dca3ca05a68b74869a
Vitaliy Shapiro <shapirovet@gmail.com>
2021-02-21 18:43:11 +0300

    Add type_of_prices initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.type_of_prices(id, name, sort_number) VALUES (default, 'Розничная цена', 1);
+INSERT INTO public.type_of_prices(id, name, sort_number) VALUES (default, 'Оптовая цена', 2);
