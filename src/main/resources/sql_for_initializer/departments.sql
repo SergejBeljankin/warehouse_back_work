13e71503eb04676c54c25a3c9a741ef73284b869
Ivan <@>
2021-02-24 10:17:01 +0300

    add sql for Department

---
+++ src/main/resources/sql_for_initializer/departments.sql
@@ -0,0 +1,16 @@
+INSERT INTO departments (name, sort_number) VALUES ('Руководство', 1);
+INSERT INTO departments (name, sort_number) VALUES ('Отдел бухгалтерии', 2);
+INSERT INTO departments (name, sort_number) VALUES ('Отдел закупок', 3);
+INSERT INTO departments (name, sort_number) VALUES ('Отдел продаж', 4);
+INSERT INTO departments (name, sort_number) VALUES ('Складской комплекс', 5);
+INSERT INTO departments (name, sort_number) VALUES ('Транспортный отдел', 6);
+INSERT INTO departments (name, sort_number) VALUES ('Финансовый отдел', 7);
+INSERT INTO departments (name, sort_number) VALUES ('Отдел маркетинга', 8);
+INSERT INTO departments (name, sort_number) VALUES ('HR отдел', 9);
+
+
+
+
+
+
+
