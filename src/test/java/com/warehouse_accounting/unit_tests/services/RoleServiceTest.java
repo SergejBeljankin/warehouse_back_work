642d7f1c38ea892c6d47a077dffec8f20145a68b
Gon4iyKot <tsapko.vladimir@gmail.com>
2021-02-24 22:50:39 +0400

    added mock behavior

--- src/test/java/com/warehouse_accounting/unit_tests/services/RoleServiceTest.java
+++ src/test/java/com/warehouse_accounting/unit_tests/services/RoleServiceTest.java
@@ -5,6 +5,8 @@ import com.warehouse_accounting.models.dto.RoleDto;
 import com.warehouse_accounting.repositories.RoleRepository;
 import com.warehouse_accounting.services.impl.RoleServiceImpl;
 import com.warehouse_accounting.util.ConverterDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
@@ -15,9 +17,15 @@ import org.springframework.util.Assert;
 
 import java.util.List;
 
+import static org.mockito.Mockito.when;
+
 
 @SpringBootTest
 class RoleServiceTest {
+
+    private static RoleDto roleDto1;
+    private static RoleDto roleDto2;
+    private static List<RoleDto> roleDtoList;
 
     @InjectMocks
     private RoleServiceImpl roleService;
@@ -25,17 +33,35 @@ class RoleServiceTest {
     @Mock
     private RoleRepository roleRepository;
 
+    @BeforeAll
+    static void initMethod() {
+        roleDto1 = RoleDto.builder()
+                .id((long)1)
+                .sortNumber("1")
+                .name("first")
+                .build();
+        roleDto2 = RoleDto.builder()
+                .id((long)2)
+                .sortNumber("2")
+                .name("second")
+                .build();
+        roleDtoList = List.of(roleDto1, roleDto2);
+    }
+
     @Test
     void getAll() {
-        List<RoleDto> roleDtoList = roleService.getAll();
-        Assert.notNull(roleDtoList, "а тут вылез null");
+        when(roleRepository.getAll()).thenReturn(roleDtoList);
+        List<RoleDto> resultRoleDtoList = roleService.getAll();
+        Assertions.assertEquals(resultRoleDtoList, roleDtoList);
+        Assert.notNull(resultRoleDtoList, "а тут вылез null");
         Mockito.verify(roleRepository, Mockito.times(1))
                 .getAll();
     }
 
     @Test
     void getById() {
-        roleService.getById((long) 1);
+        when(roleRepository.getById((long)1)).thenReturn(roleDto1);
+        Assertions.assertEquals(roleService.getById((long) 1), roleDto1);
         Mockito.verify(roleRepository, Mockito.times(1))
                 .getById(ArgumentMatchers.eq((long) 1));
 
@@ -43,18 +69,16 @@ class RoleServiceTest {
 
     @Test
     void create() {
-        RoleDto roleDto = new RoleDto();
-        Role role = ConverterDto.convertToModel(roleDto);
-        roleService.create(roleDto);
+        Role role = ConverterDto.convertToModel(roleDto1);
+        roleService.create(roleDto1);
         Mockito.verify(roleRepository, Mockito.times(1))
                 .save(ArgumentMatchers.eq(role));
     }
 
     @Test
     void update() {
-        RoleDto roleDto = new RoleDto();
-        Role role = ConverterDto.convertToModel(roleDto);
-        roleService.update(roleDto);
+        Role role = ConverterDto.convertToModel(roleDto2);
+        roleService.update(roleDto2);
         Mockito.verify(roleRepository, Mockito.times(1))
                 .save(ArgumentMatchers.eq(role));
     }
