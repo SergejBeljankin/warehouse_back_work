8be35289422a597602ac0bd08d4df6d602ebef75
Vitaliy Shapiro <shapirovet@gmail.com>
2021-02-24 13:00:50 +0300

    edit sql file

--- src/main/resources/sql_for_initializer/type_of_prices.sql
+++ src/main/resources/sql_for_initializer/type_of_prices.sql
@@ -1,2 +1,2 @@
-INSERT INTO public.type_of_prices(id, name, sort_number) VALUES (default, 'Розничная цена', 1);
-INSERT INTO public.type_of_prices(id, name, sort_number) VALUES (default, 'Оптовая цена', 2);
+INSERT INTO type_of_prices(name, sort_number) VALUES ('Розничная цена', 1);
+INSERT INTO type_of_prices(name, sort_number) VALUES ('Оптовая цена', 2);
