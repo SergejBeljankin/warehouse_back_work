6abde2fd14d93f2fa8e2dac4fa84be5b44f6fdfd
Aleksandr Aksenov <aks.aliaksandr@gmail.com>
2021-03-03 17:52:00 +0300

    add_Product_model

--- src/main/java/com/warehouse_accounting/models/Product.java
+++ src/main/java/com/warehouse_accounting/models/Product.java
@@ -1,6 +1,7 @@
 package com.warehouse_accounting.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -21,6 +22,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "products")
 public class Product {
@@ -44,33 +46,26 @@ public class Product {
     @Column
     private String description;
 
-
     @ManyToOne(fetch = FetchType.LAZY)
     private Unit unit;
 
     @Column
     private Boolean archive = false;
 
-
     @ManyToOne(fetch = FetchType.LAZY)
     private Contractor contractor;
 
-
-    @OneToMany(fetch = FetchType.LAZY)
-    private List<TypeOfPrice> typeOfPrices;
-
+//    @OneToMany(fetch = FetchType.LAZY)
+//    private List<TypeOfPrice> typeOfPrices;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private TaxSystem taxSystem;
 
-
     @ManyToMany(fetch = FetchType.LAZY)
     private List<Image> images;
 
-
     @ManyToOne(fetch = FetchType.LAZY)
     private ProductGroup productGroup;
-
 
     @ManyToOne(fetch = FetchType.LAZY)
     private AttributeOfCalculationObject attributeOfCalculationObject;
