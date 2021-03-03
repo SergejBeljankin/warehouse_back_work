576ff37fdec0307c8f17848fc1ac8b62993c90bd
illilois <mfattahov@gmail.com>
2021-03-03 22:06:21 +0300

    Added Invoice class

---
+++ src/main/java/com/warehouse_accounting/models/Invoice.java
@@ -0,0 +1,87 @@
+package com.warehouse_accounting.models;
+
+import com.warehouse_accounting.models.Company;
+import com.warehouse_accounting.models.Employee;
+import com.warehouse_accounting.models.TypeOfInvoice;
+import com.warehouse_accounting.models.Warehouse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+//import com.warehouse_accounting.models.InvoiceProduct;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+
+// Универсальная сущность для операций "ПРИХОД", "РАСХОД", "ОПРИХОДОВАНИЕ", "СПИСАНИЕ"
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "invoices")
+public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column
+    private LocalDateTime invoiceDateTime;
+
+    @Column
+    private TypeOfInvoice type;
+
+    @Column
+    private boolean isPosted;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Employee invoiceOwner;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+
+    // Сущность Project ещё не реализована!
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Warehouse warehouse;
+
+    // Сущность InvoiceProduct ещё не реализована!
+//    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+//    @JoinTable(name = "products_in_invoices", joinColumns = @JoinColumn(name = "invoice_id"), inverseJoinColumns = @JoinColumn(name = "invoice_product_id"))
+//    private Set<InvoiceProduct> products;
+
+    @Column
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Contractor contractor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Contract contract;
+
+    @Column(scale = 2)
+    private BigDecimal subTotal;
+
+    @Column(scale = 2)
+    private BigDecimal total;
+}
