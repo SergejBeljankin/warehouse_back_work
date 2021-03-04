0a2d7e1c68e62fb59ddf9f9efd185f6355148fc8
Aleksandr Aksenov <aks.aliaksandr@gmail.com>
2021-03-04 11:31:28 +0300

    1. added Project model

---
+++ src/main/java/com/warehouse_accounting/models/Project.java
@@ -0,0 +1,35 @@
+package com.warehouse_accounting.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String code;
+
+    @Column
+    private String description;
+}
