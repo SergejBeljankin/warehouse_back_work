36f59a93f9a09636a18d2bae9c26d12a418dbf40
Aleksandr Aksenov <aks.aliaksandr@gmail.com>
2021-03-05 11:05:30 +0300

    added InvoiceProductDto

---
+++ src/main/java/com/warehouse_accounting/models/dto/InvoiceProductDto.java
@@ -0,0 +1,41 @@
+package com.warehouse_accounting.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InvoiceProductDto {
+
+    private Long id;
+
+    private InvoiceDto invoiceDto = new InvoiceDto();
+
+    private ProductDto productDto = new ProductDto();
+
+    private BigDecimal count;
+
+    private BigDecimal price;
+
+    private BigDecimal sum = count.multiply(price);
+
+    public InvoiceProductDto (Long id,
+                              Long invoiceDtoId,
+                              Long productDtoId,
+                              BigDecimal count,
+                              BigDecimal price,
+                              BigDecimal sum) {
+        this.id = id;
+        this.invoiceDto.setId(invoiceDtoId);
+        this.productDto.setId(productDtoId);
+        this.count = count;
+        this.price = price;
+        this.sum = sum;
+    }
+}
