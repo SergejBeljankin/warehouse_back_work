43b8ef77d640b52f03f79d87f7dad5dea46fa218
User <anatienko.yv@gmail.com>
2021-03-10 01:09:21 +0300

    add filter to LegalDetailRestController

--- src/main/java/com/warehouse_accounting/WarehouseAccountingApplication.java
+++ src/main/java/com/warehouse_accounting/WarehouseAccountingApplication.java
@@ -1,10 +1,20 @@
 package com.warehouse_accounting;
 
+import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
-public class WarehouseAccountingApplication {
+public class WarehouseAccountingApplication implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new SpecificationArgumentResolver());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WarehouseAccountingApplication.class, args);
--- src/main/java/com/warehouse_accounting/controllers/rest/LegalDetailRestController.java
+++ src/main/java/com/warehouse_accounting/controllers/rest/LegalDetailRestController.java
@@ -1,14 +1,23 @@
 package com.warehouse_accounting.controllers.rest;
 
+import com.warehouse_accounting.models.LegalDetail;
 import com.warehouse_accounting.models.dto.LegalDetailDto;
 import com.warehouse_accounting.services.interfaces.CheckEntityService;
 import com.warehouse_accounting.services.interfaces.LegalDetailService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import net.kaczmarzyk.spring.data.jpa.domain.Between;
+import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Join;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,10 +47,10 @@ public class LegalDetailRestController {
     @GetMapping
     @ApiOperation(value = "Возвращает список по юрдической информации", notes = "Возвращает список LegalDetailDto", response = LegalDetailDto.class)
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description  = "Успешное получение листа юрдической информациии"),
-            @ApiResponse(responseCode = "404", description  = "Данный контролер не найден"),
-            @ApiResponse(responseCode = "403", description  = "Операция запрещена"),
-            @ApiResponse(responseCode = "401", description  = "Нет доступа к данной операции")}
+            @ApiResponse(responseCode = "200", description = "Успешное получение листа юрдической информациии"),
+            @ApiResponse(responseCode = "404", description = "Данный контролер не найден"),
+            @ApiResponse(responseCode = "403", description = "Операция запрещена"),
+            @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
     )
     public ResponseEntity<List<LegalDetailDto>> getAll() {
         return ResponseEntity.ok(legalDetailService.getAll());
@@ -50,10 +59,10 @@ public class LegalDetailRestController {
     @GetMapping("/{id}")
     @ApiOperation(value = "Возвращает юрдическую информацию по id", notes = "Возвращает LegalDetailDto", response = LegalDetailDto.class)
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description  = "Успешное получение юридической информации"),
-            @ApiResponse(responseCode = "404", description  = "Данный контролер не найден"),
-            @ApiResponse(responseCode = "403", description  = "Операция запрещена"),
-            @ApiResponse(responseCode = "401", description  = "Нет доступа к данной операции")}
+            @ApiResponse(responseCode = "200", description = "Успешное получение юридической информации"),
+            @ApiResponse(responseCode = "404", description = "Данный контролер не найден"),
+            @ApiResponse(responseCode = "403", description = "Операция запрещена"),
+            @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
     )
     public ResponseEntity<LegalDetailDto> getById(
             @ApiParam(name = "id", value = "Значение поля Id объекта которого хотим получить", example = "1", required = true)
@@ -65,10 +74,10 @@ public class LegalDetailRestController {
     @PutMapping
     @ApiOperation(value = "Создает юрдическую информацию")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description  = "Успешное создание юридической информации"),
-            @ApiResponse(responseCode = "404", description  = "Данный контролер не найден"),
-            @ApiResponse(responseCode = "403", description  = "Операция запрещена"),
-            @ApiResponse(responseCode = "401", description  = "Нет доступа к данной операции")}
+            @ApiResponse(responseCode = "200", description = "Успешное создание юридической информации"),
+            @ApiResponse(responseCode = "404", description = "Данный контролер не найден"),
+            @ApiResponse(responseCode = "403", description = "Операция запрещена"),
+            @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
     )
     public ResponseEntity<?> create(
             @ApiParam(name = "LegalDetailDto", value = "Объект LegalDetailDto который нужно обновить в программе")
@@ -80,10 +89,10 @@ public class LegalDetailRestController {
     @PostMapping
     @ApiOperation(value = "Изменяет юрдическую информацию")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description  = "Успешное изменение юридической информации"),
-            @ApiResponse(responseCode = "404", description  = "Данный контролер не найден"),
-            @ApiResponse(responseCode = "403", description  = "Операция запрещена"),
-            @ApiResponse(responseCode = "401", description  = "Нет доступа к данной операции")}
+            @ApiResponse(responseCode = "200", description = "Успешное изменение юридической информации"),
+            @ApiResponse(responseCode = "404", description = "Данный контролер не найден"),
+            @ApiResponse(responseCode = "403", description = "Операция запрещена"),
+            @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
     )
     public ResponseEntity<?> update(
             @ApiParam(name = "LegalDetailDto", value = "Объект LegalDetailDto который нужно сохранить в программе")
@@ -96,10 +105,10 @@ public class LegalDetailRestController {
     @DeleteMapping("/{id}")
     @ApiOperation(value = "Удаляет юрдическую информацию по id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description  = "Успешное удаление юридической информации"),
-            @ApiResponse(responseCode = "404", description  = "Данный контролер не найден"),
-            @ApiResponse(responseCode = "403", description  = "Операция запрещена"),
-            @ApiResponse(responseCode = "401", description  = "Нет доступа к данной операции")}
+            @ApiResponse(responseCode = "200", description = "Успешное удаление юридической информации"),
+            @ApiResponse(responseCode = "404", description = "Данный контролер не найден"),
+            @ApiResponse(responseCode = "403", description = "Операция запрещена"),
+            @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
     )
     public ResponseEntity<?> deleteById(
             @ApiParam(name = "id", value = "Значение поля Id объекта который хотим удалить", example = "1", required = true)
@@ -107,5 +116,45 @@ public class LegalDetailRestController {
         checkEntityService.checkExistLegalDetailById(id);
         legalDetailService.deleteById(id);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/filter")
+    @ApiOperation(value = "Возвращает отфильтрованный список по юрдической информации", notes = "Возвращает отфильтрованный список LegalDetailDto", response = LegalDetailDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное получение листа юрдической информациии"),
+            @ApiResponse(responseCode = "404", description = "Данный контролер не найден"),
+            @ApiResponse(responseCode = "403", description = "Операция запрещена"),
+            @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")}
+    )
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "lastName", value = "Значение поля lastName объекта по которому хотим отфильтровать, example: String", dataType = "String", paramType = "query"),
+            @ApiImplicitParam(name = "firstName", value = "Значение поля firstName объекта по которому хотим отфильтровать, example: String", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "middleName", value = "Значение поля middleName объекта по которому хотим отфильтровать, example: String", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "commentToAddress", value = "Значение поля commentToAddress объекта по которому хотим отфильтровать, example: String", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "inn", value = "Значение поля inn объекта по которому хотим отфильтровать, example: String", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "okpo", value = "Значение поля okpo объекта по которому хотим отфильтровать, example: String", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "ogrnip", value = "Значение поля ogrnip объекта по которому хотим отфильтровать, example: String", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "numberOfTheCertificate", value = "Значение поля ogrnip объекта по которому хотим отфильтровать, example: String", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "dateOfTheCertificateBetween", value = "Значение поля dateOfTheCertificate c/по дату для фильтрации, example: 2021-03-01;2021-03-10", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "dateOfTheCertificateEquals", value = "Значение поля dateOfTheCertificate объекта по которому хотим отфильтровать, example: 2021-03-08", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "typeOfContractorName", value = "Значение поля typeOfContractor - name объекта по которому хотим отфильтровать, example: string", dataType = "string", paramType = "query"),
+            @ApiImplicitParam(name = "typeOfContractorId", value = "Значение поля typeOfContractor - id объекта по которому хотим отфильтровать, example: 1", dataType = "string", paramType = "query")
+    })
+    public ResponseEntity<List<LegalDetailDto>> filterBySpecification(
+            @Join(path = "typeOfContractor", alias = "toc")
+            @And({
+                    @Spec(path = "lastName", spec = Equal.class),
+                    @Spec(path = "middleName", spec = Equal.class),
+                    @Spec(path = "commentToAddress", spec = Equal.class),
+                    @Spec(path = "inn", spec = Equal.class),
+                    @Spec(path = "okpo", spec = Equal.class),
+                    @Spec(path = "ogrnip", spec = Equal.class),
+                    @Spec(path = "numberOfTheCertificate", spec = Equal.class),
+                    @Spec(path = "dateOfTheCertificate", params = "dateOfTheCertificateBetween", paramSeparator = '\u003B', config = "yyyy-MM-dd", spec = Between.class),
+                    @Spec(path = "dateOfTheCertificate", params = "dateOfTheCertificateEquals", config = "yyyy-MM-dd", spec = Equal.class),
+                    @Spec(path = "toc.name", params = "typeOfContractorName", spec = Equal.class),
+                    @Spec(path = "toc.id", params = "typeOfContractorId", spec = Equal.class)
+            }) Specification<LegalDetail> specification) {
+        return ResponseEntity.ok(legalDetailService.getAllBySpecification(specification));
     }
 }
