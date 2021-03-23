eeed501b3b9ad29b3f5b8f38d3dc10b8bc887ed3
illilois <mfattahov@gmail.com>
2021-03-23 16:13:17 +0300

    Lightened InvoiceDto; updated InvoiceRepository, InvoiceServiceImpl, ConverterDto,

--- src/main/java/com/warehouse_accounting/repositories/InvoiceRepository.java
+++ src/main/java/com/warehouse_accounting/repositories/InvoiceRepository.java
@@ -8,37 +8,69 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
-    @Query("SELECT NEW com.warehouse_accounting.models.dto.InvoiceDto(i.id, " +
+    @Query("SELECT NEW com.warehouse_accounting.models.dto.InvoiceDto(" +
+            "i.id, " +
             "i.number, " +
             "i.invoiceDateTime, " +
             "i.type, " +
             "i.isPosted, " +
-            "i.invoiceAuthor.id, " +
-            "i.company.id, " +
-            "i.project.id, " +
-            "i.warehouse.id, " +
-            "i.comment, " +
-            "i.contractor.id, " +
-            "i.contract.id)" +
-            "FROM Invoice i")
+            "e.id, " +
+            "e.lastName," +
+            "e.firstName," +
+            "c.id," +
+            "c.name, " +
+            "p.id," +
+            "p.name, " +
+            "w.id," +
+            "w.name, " +
+            "i.comment," +
+            "con.id, " +
+            "con.name, " +
+            "cont.id," +
+            "cont.number, " +
+            "cont.contractDate)" +
+            "FROM Invoice i " +
+            "LEFT JOIN Employee e ON (i.invoiceAuthor.id = e.id)" +
+            "LEFT JOIN Company c ON (i.company.id = c.id)" +
+            "LEFT JOIN Project p ON (i.project.id = p.id)" +
+            "LEFT JOIN Warehouse w ON (i.warehouse.id = w.id)" +
+            "LEFT JOIN Contractor con ON (i.contractor.id = con.id)" +
+            "LEFT JOIN Contract cont ON (i.contract.id = cont.id)")
     List<InvoiceDto> getAll();
 
-    @Query("SELECT NEW com.warehouse_accounting.models.dto.InvoiceDto(i.id, " +
+    @Query("SELECT NEW com.warehouse_accounting.models.dto.InvoiceDto(" +
+            "i.id, " +
             "i.number, " +
             "i.invoiceDateTime, " +
             "i.type, " +
             "i.isPosted, " +
-            "i.invoiceAuthor.id, " +
-            "i.company.id, " +
-            "i.project.id, " +
-            "i.warehouse.id, " +
-            "i.comment, " +
-            "i.contractor.id, " +
-            "i.contract.id)" +
-            "FROM Invoice i WHERE i.id = :id")
+            "e.id, " +
+            "e.lastName," +
+            "e.firstName," +
+            "c.id," +
+            "c.name, " +
+            "p.id," +
+            "p.name, " +
+            "w.id," +
+            "w.name, " +
+            "i.comment," +
+            "con.id, " +
+            "con.name, " +
+            "cont.id," +
+            "cont.number, " +
+            "cont.contractDate)" +
+            "FROM Invoice i " +
+            "LEFT JOIN Employee e ON (i.invoiceAuthor.id = e.id)" +
+            "LEFT JOIN Company c ON (i.company.id = c.id)" +
+            "LEFT JOIN Project p ON (i.project.id = p.id)" +
+            "LEFT JOIN Warehouse w ON (i.warehouse.id = w.id)" +
+            "LEFT JOIN Contractor con ON (i.contractor.id = con.id)" +
+            "LEFT JOIN Contract cont ON (i.contract.id = cont.id)" +
+            "WHERE i.id = :id")
     InvoiceDto getById(@Param("id") Long id);
 
 }
--- src/main/java/com/warehouse_accounting/services/impl/InvoiceServiceImpl.java
+++ src/main/java/com/warehouse_accounting/services/impl/InvoiceServiceImpl.java
@@ -1,15 +1,11 @@
 package com.warehouse_accounting.services.impl;
 
 import com.warehouse_accounting.models.dto.InvoiceDto;
-import com.warehouse_accounting.repositories.CompanyRepository;
 import com.warehouse_accounting.repositories.ContractRepository;
-import com.warehouse_accounting.repositories.ContractorRepository;
 import com.warehouse_accounting.repositories.EmployeeRepository;
 import com.warehouse_accounting.repositories.InvoiceEditRepository;
 import com.warehouse_accounting.repositories.InvoiceProductRepository;
 import com.warehouse_accounting.repositories.InvoiceRepository;
-import com.warehouse_accounting.repositories.ProjectRepository;
-import com.warehouse_accounting.repositories.WarehouseRepository;
 import com.warehouse_accounting.services.interfaces.InvoiceService;
 import com.warehouse_accounting.util.ConverterDto;
 import org.springframework.stereotype.Service;
@@ -22,50 +18,27 @@ import java.util.stream.Collectors;
 @Transactional
 public class InvoiceServiceImpl implements InvoiceService {
 
-    private final CompanyRepository companyRepository;
-    private final ContractorRepository contractorRepository;
-    private final ContractRepository contractRepository;
-    private final EmployeeRepository employeeRepository;
     private final InvoiceRepository invoiceRepository;
     private final InvoiceEditRepository invoiceEditRepository;
     private final InvoiceProductRepository invoiceProductRepository;
-    private final ProjectRepository projectRepository;
-    private final WarehouseRepository warehouseRepository;
 
-    public InvoiceServiceImpl(CompanyRepository companyRepository,
-                              ContractorRepository contractorRepository,
-                              ContractRepository contractRepository,
-                              EmployeeRepository employeeRepository,
-                              InvoiceRepository invoiceRepository,
+
+    public InvoiceServiceImpl(InvoiceRepository invoiceRepository,
                               InvoiceEditRepository invoiceEditRepository,
-                              InvoiceProductRepository invoiceProductRepository,
-                              ProjectRepository projectRepository,
-                              WarehouseRepository warehouseRepository) {
-        this.companyRepository = companyRepository;
-        this.contractorRepository = contractorRepository;
-        this.contractRepository = contractRepository;
-        this.employeeRepository = employeeRepository;
+                              InvoiceProductRepository invoiceProductRepository) {
         this.invoiceRepository = invoiceRepository;
         this.invoiceEditRepository = invoiceEditRepository;
         this.invoiceProductRepository = invoiceProductRepository;
-        this.projectRepository = projectRepository;
-        this.warehouseRepository = warehouseRepository;
     }
 
     @Override
     public List<InvoiceDto> getAll() {
         List<InvoiceDto> invoiceDtos = invoiceRepository.getAll();
         for (InvoiceDto invoiceDto : invoiceDtos) {
-            invoiceDto.setInvoiceAuthor(employeeRepository.getById(invoiceDto.getInvoiceAuthor().getId()));
-            invoiceDto.setCompanyDto(companyRepository.getById(invoiceDto.getCompanyDto().getId()));
-            invoiceDto.setProjectDto(projectRepository.getById(invoiceDto.getProjectDto().getId()));
-            invoiceDto.setWarehouseDto(warehouseRepository.getById(invoiceDto.getWarehouseDto().getId()));
             invoiceDto.setProductDtos(invoiceProductRepository.getListInvoiceProductById(invoiceDto.getId())
                     .stream()
                     .map(ConverterDto::convertToDto)
                     .collect(Collectors.toList()));
-            invoiceDto.setContractorDto(contractorRepository.getById(invoiceDto.getContractorDto().getId()));
-            invoiceDto.setContractDto(contractRepository.getById(invoiceDto.getContractDto().getId()));
             invoiceDto.setEdits(invoiceEditRepository.getListInvoiceEditById(invoiceDto.getId())
                     .stream()
                     .map(ConverterDto::convertToDto)
@@ -77,16 +50,10 @@ public class InvoiceServiceImpl implements InvoiceService {
     @Override
     public InvoiceDto getById(Long id) {
         InvoiceDto invoiceDto = invoiceRepository.getById(id);
-        invoiceDto.setInvoiceAuthor(employeeRepository.getById(invoiceDto.getInvoiceAuthor().getId()));
-        invoiceDto.setCompanyDto(companyRepository.getById(invoiceDto.getCompanyDto().getId()));
-        invoiceDto.setProjectDto(projectRepository.getById(invoiceDto.getProjectDto().getId()));
-        invoiceDto.setWarehouseDto(warehouseRepository.getById(invoiceDto.getWarehouseDto().getId()));
         invoiceDto.setProductDtos(invoiceProductRepository.getListInvoiceProductById(invoiceDto.getId())
                 .stream()
                 .map(ConverterDto::convertToDto)
                 .collect(Collectors.toList()));
-        invoiceDto.setContractorDto(contractorRepository.getById(invoiceDto.getContractorDto().getId()));
-        invoiceDto.setContractDto(contractRepository.getById(invoiceDto.getContractDto().getId()));
         invoiceDto.setEdits(invoiceEditRepository.getListInvoiceEditById(invoiceDto.getId())
                 .stream()
                 .map(ConverterDto::convertToDto)
