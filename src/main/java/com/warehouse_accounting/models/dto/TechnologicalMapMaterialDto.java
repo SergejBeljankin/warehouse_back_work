35da13670f113d737a38f5d47575cddde8b0cc47
Pavel Smirnov <smirnov@0x55.ru>
2021-03-26 20:58:25 +0300

    BugFix: Creat crosslink Model and DTO

--- src/main/java/com/warehouse_accounting/models/dto/TechnologicalMapMaterialDto.java
+++ src/main/java/com/warehouse_accounting/models/dto/TechnologicalMapMaterialDto.java
@@ -24,5 +24,19 @@ public class TechnologicalMapMaterialDto {
     private Long materialId;
     private String materialName;
 
-    private BigDecimal count= BigDecimal.valueOf(1);
+    private BigDecimal count = BigDecimal.valueOf(1);
+
+    private TechnologicalMapDto technologicalMapDto = new TechnologicalMapDto();
+
+    public TechnologicalMapMaterialDto(Long id,
+                                       Long materialId,
+                                       String materialName,
+                                       BigDecimal count,
+                                       Long technologicalMapDto) {
+        this.id = id;
+        this.materialId = materialId;
+        this.materialName = materialName;
+        this.count = count;
+        this.technologicalMapDto.setId(technologicalMapDto);
+    }
 }
