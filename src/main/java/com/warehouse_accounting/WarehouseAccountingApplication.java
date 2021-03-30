39395494bfe984dcb8b6e559606b23cdfe762160
Максим <sidor8maximus230@gmail.com>
2021-03-30 12:46:17 +0300

    FIX

--- src/main/java/com/warehouse_accounting/WarehouseAccountingApplication.java
+++ src/main/java/com/warehouse_accounting/WarehouseAccountingApplication.java
@@ -1,21 +1,10 @@
 package com.warehouse_accounting;
 
-import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @SpringBootApplication
-public class WarehouseAccountingApplication implements WebMvcConfigurer {
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new SpecificationArgumentResolver());
-    }
-
+public class WarehouseAccountingApplication {
     public static void main(String[] args) {
         SpringApplication.run(WarehouseAccountingApplication.class, args);
     }
