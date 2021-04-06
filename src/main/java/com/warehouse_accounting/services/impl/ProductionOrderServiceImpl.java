c1312530591477f997561d8b3e4f931e2840c20a
Charly Breadberry <aleks_inter@mail.ru>
2021-04-06 21:39:10 +0300

    change ProductionOrderDto, ProductionOrderRepository, ConverterDto

--- src/main/java/com/warehouse_accounting/services/impl/ProductionOrderServiceImpl.java
+++ src/main/java/com/warehouse_accounting/services/impl/ProductionOrderServiceImpl.java
@@ -1,19 +1,15 @@
 package com.warehouse_accounting.services.impl;
 
-import com.warehouse_accounting.models.Company;
-import com.warehouse_accounting.models.ProductionOrder;
-import com.warehouse_accounting.models.dto.InvoiceEditDto;
 import com.warehouse_accounting.models.dto.ProductionOrderDto;
 import com.warehouse_accounting.repositories.ProductionOrderRepository;
-import com.warehouse_accounting.repositories.TechnologicalMapRepository;
+
 import com.warehouse_accounting.services.interfaces.ProductionOrderService;
 import com.warehouse_accounting.util.ConverterDto;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Service
@@ -21,21 +17,15 @@ import java.util.List;
 public class ProductionOrderServiceImpl implements ProductionOrderService {
 
     private final ProductionOrderRepository productionOrderRepository;
-    private final TechnologicalMapRepository technologicalMapRepository;
 
-    public ProductionOrderServiceImpl(ProductionOrderRepository productionOrderRepository,
-                                      TechnologicalMapRepository technologicalMapRepository) {
+    public ProductionOrderServiceImpl(ProductionOrderRepository productionOrderRepository) {
         this.productionOrderRepository = productionOrderRepository;
-        this.technologicalMapRepository = technologicalMapRepository;
     }
 
 
     @Override
     public List<ProductionOrderDto> getAll() {
         List<ProductionOrderDto> productionOrderDtoList = productionOrderRepository.getAll();
-        for (ProductionOrderDto order : productionOrderDtoList) {
-            order.setTechMapDto(technologicalMapRepository.getById(order.getTechMapDto().getId()));
-        }
         return productionOrderDtoList;
 
     }
@@ -43,7 +33,6 @@ public class ProductionOrderServiceImpl implements ProductionOrderService {
     @Override
     public ProductionOrderDto getById(Long id) {
         ProductionOrderDto productionOrderDto = productionOrderRepository.getById(id);
-        productionOrderDto.setTechMapDto(technologicalMapRepository.getById(productionOrderDto.getTechMapDto().getId()));
         return productionOrderDto;
     }
 
