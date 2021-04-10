8db3506cf4f7b0561f2de19299c0f603b11fc050
Pavel Smirnov <smirnov@0x55.ru>
2021-04-10 18:56:19 +0300

    Add Task DTO

---
+++ src/main/java/com/warehouse_accounting/models/dto/TaskDto.java
@@ -0,0 +1,42 @@
+package com.warehouse_accounting.models.dto;
+
+import com.warehouse_accounting.models.Contractor;
+import com.warehouse_accounting.models.Document;
+import com.warehouse_accounting.models.Employee;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+/**
+ * This DTO class for {@link com.warehouse_accounting.models.Task}
+ *
+ * @author pavelsmirnov
+ * @version 0.1
+ * Created 10.04.2021
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TaskDto {
+    private Long id;
+    private String description;
+    private LocalDateTime deadline;
+    private LocalDateTime dateOfCreation;
+
+    private Long executorId;
+    private String executorName;
+
+    private Boolean isDone = false;
+
+    private Long contractorId;
+    private String contractorName;
+
+    private Long documentId;
+    private String documentName;
+}
