2dd5c1d3d7aa0dfb4db4a520da44ba8a551f06be
pan-s <pan-s1998@bk.ru>
2021-05-16 22:17:27 +0300

    Added annotation Repository to PaymentRepository

--- src/main/java/com/warehouse_accounting/repositories/PaymentRepository.java
+++ src/main/java/com/warehouse_accounting/repositories/PaymentRepository.java
@@ -5,9 +5,11 @@ import com.warehouse_accounting.models.dto.PaymentDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
 
     @Query("SELECT NEW com.warehouse_accounting.models.dto.PaymentDto(" +
