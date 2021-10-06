7e38a40e6e2ccee748400f7ceca29706ef97a626
At0m616 <At0m616@yandex.ru>
2021-10-06 20:37:02 +0500

    Добавлен ProductGroupRestControllerTest. добавлены данные в DataInitializer. Исправлен ProductGroup

--- src/main/java/com/warehouse_accounting/configs/DataInitializer.java
+++ src/main/java/com/warehouse_accounting/configs/DataInitializer.java
@@ -2,56 +2,8 @@ package com.warehouse_accounting.configs;
 
 import com.warehouse_accounting.models.TypeOfAdjustment;
 import com.warehouse_accounting.models.TypeOfPayment;
-import com.warehouse_accounting.models.dto.AdjustmentDto;
-import com.warehouse_accounting.models.dto.BankAccountDto;
-import com.warehouse_accounting.models.dto.CallDto;
-import com.warehouse_accounting.models.dto.CompanyDto;
-import com.warehouse_accounting.models.dto.ContractDto;
-import com.warehouse_accounting.models.dto.ContractorDto;
-import com.warehouse_accounting.models.dto.ContractorGroupDto;
-import com.warehouse_accounting.models.dto.DepartmentDto;
-import com.warehouse_accounting.models.dto.EmployeeDto;
-import com.warehouse_accounting.models.dto.ImageDto;
-import com.warehouse_accounting.models.dto.LegalDetailDto;
-import com.warehouse_accounting.models.dto.PaymentDto;
-import com.warehouse_accounting.models.dto.PositionDto;
-import com.warehouse_accounting.models.dto.ProductDto;
-import com.warehouse_accounting.models.dto.ProjectDto;
-import com.warehouse_accounting.models.dto.RoleDto;
-import com.warehouse_accounting.models.dto.TaskDto;
-import com.warehouse_accounting.models.dto.TechnologicalMapDto;
-import com.warehouse_accounting.models.dto.TechnologicalMapGroupDto;
-import com.warehouse_accounting.models.dto.TechnologicalMapMaterialDto;
-import com.warehouse_accounting.models.dto.TechnologicalMapProductDto;
-import com.warehouse_accounting.models.dto.TechnologicalOperationDto;
-import com.warehouse_accounting.models.dto.TypeOfContractorDto;
-import com.warehouse_accounting.models.dto.TypeOfPriceDto;
-import com.warehouse_accounting.models.dto.UnitDto;
-import com.warehouse_accounting.services.interfaces.AdjustmentService;
-import com.warehouse_accounting.services.interfaces.BankAccountService;
-import com.warehouse_accounting.services.interfaces.CallService;
-import com.warehouse_accounting.services.interfaces.CompanyService;
-import com.warehouse_accounting.services.interfaces.ContractService;
-import com.warehouse_accounting.services.interfaces.ContractorGroupService;
-import com.warehouse_accounting.services.interfaces.ContractorService;
-import com.warehouse_accounting.services.interfaces.DepartmentService;
-import com.warehouse_accounting.services.interfaces.EmployeeService;
-import com.warehouse_accounting.services.interfaces.ImageService;
-import com.warehouse_accounting.services.interfaces.LegalDetailService;
-import com.warehouse_accounting.services.interfaces.PaymentService;
-import com.warehouse_accounting.services.interfaces.PositionService;
-import com.warehouse_accounting.services.interfaces.ProductService;
-import com.warehouse_accounting.services.interfaces.ProjectService;
-import com.warehouse_accounting.services.interfaces.RoleService;
-import com.warehouse_accounting.services.interfaces.TaskService;
-import com.warehouse_accounting.services.interfaces.TechnologicalMapGroupService;
-import com.warehouse_accounting.services.interfaces.TechnologicalMapMaterialService;
-import com.warehouse_accounting.services.interfaces.TechnologicalMapProductService;
-import com.warehouse_accounting.services.interfaces.TechnologicalMapService;
-import com.warehouse_accounting.services.interfaces.TechnologicalOperationService;
-import com.warehouse_accounting.services.interfaces.TypeOfContractorService;
-import com.warehouse_accounting.services.interfaces.TypeOfPriceService;
-import com.warehouse_accounting.services.interfaces.UnitService;
+import com.warehouse_accounting.models.dto.*;
+import com.warehouse_accounting.services.interfaces.*;
 import lombok.extern.log4j.Log4j2;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -101,6 +53,7 @@ public class DataInitializer {
     private final CompanyService companyService;
     private final ContractService contractService;
     private final PaymentService paymentService;
+    private final ProductGroupService productGroupService;
 
     public DataInitializer(RoleService roleService,
                            UnitService unitService,
@@ -126,7 +79,8 @@ public class DataInitializer {
                            ProjectService projectService,
                            CompanyService companyService,
                            ContractService contractService,
-                           PaymentService paymentService) {
+                           PaymentService paymentService,
+                           ProductGroupService productGroupService) {
         this.roleService = roleService;
         this.unitService = unitService;
         this.productService = productService;
@@ -152,6 +106,7 @@ public class DataInitializer {
         this.companyService = companyService;
         this.contractService = contractService;
         this.paymentService = paymentService;
+        this.productGroupService = productGroupService;
     }
 
     @PostConstruct
@@ -178,6 +133,7 @@ public class DataInitializer {
         initAdjustments();
         initContract();
         initPayment();
+        initProductGroup();
     }
 
     private void initRoles() {
@@ -742,6 +698,31 @@ public class DataInitializer {
         } catch (Exception e) {
             log.error("Не удалось заполнить таблицу projects", e);
         }
+    }
+
+    private void initProductGroup(){
+        try {
+            productGroupService.create(ProductGroupDto.builder()
+                    .id(1L)
+                    .name("Parent Product")
+                    .sortNumber("42")
+                    .build());
+            productGroupService.create(ProductGroupDto.builder()
+                    .id(2L)
+                    .name("Product2")
+                    .sortNumber("242")
+                    .parentId(1L)
+                    .build());
+            productGroupService.create(ProductGroupDto.builder()
+                    .id(3L)
+                    .name("Product3")
+                    .sortNumber("342")
+                    .parentId(1L)
+                    .build());
+        } catch (Exception e) {
+            log.error("Не удалось заполнить таблицу product_groups", e);
+        }
+
     }
 
 }
---
+++ src/test/java/com/warehouse_accounting/integration_tests/ProductGroupRestControllerTest.java
@@ -0,0 +1,116 @@
+package com.warehouse_accounting.integration_tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.warehouse_accounting.controllers.rest.ProductGroupRestController;
+import com.warehouse_accounting.models.dto.ProductGroupDto;
+import com.warehouse_accounting.services.interfaces.ProductGroupService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductGroupRestControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ProductGroupRestController productGroupRestController;
+    @Autowired
+    private ProductGroupService productGroupService;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testExist() {
+        assertThat(productGroupRestController).isNotNull();
+    }
+
+    @Test
+    void getAll() throws Exception {
+        mockMvc.perform(get("/api/product_groups"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getById() throws Exception {
+        mockMvc.perform(get("/api/product_groups/2"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("2"))
+                .andExpect(jsonPath("$.name").value("Product2"))
+                .andExpect(jsonPath("$.sortNumber").value("242"))
+                .andExpect(jsonPath("$.parentId").value(1));
+    }
+
+    @Test
+    void create() throws Exception {
+        ProductGroupDto dto = ProductGroupDto.builder()
+                .id(4L)
+                .name("TestProduct")
+                .sortNumber("sortNumber")
+                .parentId(1L)
+                .build();
+
+        String jsonAdjustment = new ObjectMapper().writeValueAsString(dto);
+        mockMvc.perform(post("/api/product_groups")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonAdjustment))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/product_groups/4"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("4"))
+                .andExpect(jsonPath("$.name").value("TestProduct"))
+                .andExpect(jsonPath("$.sortNumber").value("sortNumber"))
+                .andExpect(jsonPath("$.parentId").value(1));
+    }
+
+    @Test
+    void update() throws Exception {
+        ProductGroupDto dto = productGroupService.getById(1L);
+        dto.setName("Update name");
+        dto.setSortNumber("Update number");
+
+        mockMvc.perform(get("/api/product_groups/1"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/product_groups")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/product_groups/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(dto)));
+    }
+
+    @Test
+    void deleteById() throws Exception {
+        mockMvc.perform(get("/api/product_groups/3"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        mockMvc.perform(delete("/api/product_groups/3"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/product_groups/3"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+}
