3d81f25421db1113c270046967aa1e3340f02d7d
BulatRinch <rinchb@internet.ru>
2021-10-11 18:45:46 +0700

    Back: Создать Model и DTO -реализация блока "Пользователь/Настройки Пользователя"+repository(in progress)

---
+++ src/main/java/com/warehouse_accounting/repositories/Settings.java
@@ -0,0 +1,7 @@
+package com.warehouse_accounting.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Settings extends JpaRepository<com.warehouse_accounting.models.Settings, Long> {
+
+}
