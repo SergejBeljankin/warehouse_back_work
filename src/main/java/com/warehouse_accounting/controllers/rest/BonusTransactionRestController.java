9881d4bd02d69c172e7daee97faed041e11cc5e5
pavel203 <pavel.tarasov799@gmail.com>
2021-10-14 18:55:02 +0300

    ***

--- src/main/java/com/warehouse_accounting/controllers/rest/BonusTransactionRestController.java
+++ src/main/java/com/warehouse_accounting/controllers/rest/BonusTransactionRestController.java
@@ -1,14 +1,14 @@
 package com.warehouse_accounting.controllers.rest;
 
-
 import com.warehouse_accounting.models.dto.BonusTransactionDto;
-import com.warehouse_accounting.services.interfaces.BonusTransactionService;
+import com.warehouse_accounting.services.impl.BonusTransactionServiceImpl;
 import com.warehouse_accounting.services.interfaces.CheckEntityService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,14 +21,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/bonus_transactions")
 public class BonusTransactionRestController {
-    private final BonusTransactionService bonusTransactionService;
 
     private final CheckEntityService checkEntityService;
+    private final BonusTransactionServiceImpl bonusTransactionService;
 
-    public BonusTransactionRestController(BonusTransactionService bonusTransactionService,
-                                          CheckEntityService checkEntityService) {
-        this.bonusTransactionService = bonusTransactionService;
+    public BonusTransactionRestController(CheckEntityService checkEntityService,
+                                          BonusTransactionServiceImpl bonusTransactionService) {
         this.checkEntityService = checkEntityService;
+        this.bonusTransactionService = bonusTransactionService;
     }
 
     @GetMapping
@@ -40,11 +40,8 @@ public class BonusTransactionRestController {
             @ApiResponse(responseCode = "403", description = "Операция запрещена"),
             @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")})
     public ResponseEntity<List<BonusTransactionDto>> getAll() {
-        return ResponseEntity.ok(null);
+        return ResponseEntity.ok(bonusTransactionService.getAll());
     }
-
-
-
 
     @GetMapping("/{id}")
     @ApiOperation(value = "Возвращает объект BonusTransactionDto",
@@ -57,8 +54,8 @@ public class BonusTransactionRestController {
     public ResponseEntity<BonusTransactionDto> getById(@ApiParam(name =
             "id", value = "Id нужного BonusTransactionDto", required = true)
                                            @PathVariable("id") Long id) {
-        //checkEntityService.checkExistBonusTransactionDto(id);
-        return ResponseEntity.ok(null);
+        checkEntityService.checkExistBonusTransactionById(id);
+        return ResponseEntity.ok(bonusTransactionService.getById(id));
     }
 
     @PostMapping
@@ -71,7 +68,7 @@ public class BonusTransactionRestController {
             @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")})
     public ResponseEntity<?> create(@ApiParam(name = "BonusTransactionDto", value = "Объект BonusTransactionDto для создания",
             required = true) @RequestBody BonusTransactionDto bonusTransactionDto) {
-        //bonusTransactionService.create(bonusTransactionDto);
+        bonusTransactionService.create(bonusTransactionDto);
         return ResponseEntity.ok().build();
     }
 
@@ -85,8 +82,24 @@ public class BonusTransactionRestController {
             @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")})
     public ResponseEntity<?> update(@ApiParam(name = "BonusTransactionDto", value = "Объект BonusTransactionDto для обновления",
             required = true) @RequestBody BonusTransactionDto bonusTransactionDto) {
-        //bonusTransactionService.create(bonusTransactionDto.getId());
-        //bonusTransactionService.update(bonusTransactionDto);
+        checkEntityService.checkExistBonusTransactionById(bonusTransactionDto.getId());
+        bonusTransactionService.update(bonusTransactionDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Удаляет объект BonusTransactionDto с выбранным id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Запрос успешно выполнен"),
+            @ApiResponse(responseCode = "204", description = "Cервер успешно обработал запрос, но в ответе были переданы только заголовки без тела сообщения"),
+            @ApiResponse(responseCode = "404", description = "Данный контролер не найден"),
+            @ApiResponse(responseCode = "403", description = "Операция запрещена"),
+            @ApiResponse(responseCode = "401", description = "Нет доступа к данной операции")})
+    public ResponseEntity<?> deleteById(@ApiParam(name =
+            "id", value = "Id BonusTransactionDto для удаления", required = true)
+                                        @PathVariable("id") Long id) {
+        checkEntityService.checkExistBonusTransactionById(id);
+        bonusTransactionService.deleteById(id);
         return ResponseEntity.ok().build();
     }
 
